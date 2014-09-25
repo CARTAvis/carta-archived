6855ec57ddb4640456e393c4c8b2ff5293a83e23
Pavol Federl <federl@gmail.com>
2014-09-24 23:31:01 -0600

    added a trivial image cache
    this makes switching frames much faster once a frame was already visited

--- Skeleton3/cpp/common/Viewer.cpp
+++ Skeleton3/cpp/common/Viewer.cpp
@@ -12,14 +12,78 @@
 #include <QColor>
 #include <QPainter>
 #include <QDebug>
+#include <QCache>
 #include <QCoreApplication>
 #include <cmath>
 #include <iostream>
+#include <limits>
+
+/// compute clip values from the values in a view
+template <typename Scalar>
+static
+typename std::tuple<Scalar,Scalar> computeClips(
+        NdArray::TypedView<Scalar> & view,
+        double perc
+        )
+{
+    // read in all values from the view into an array
+    // we need our own copy because we'll do quickselect on it...
+    std::vector<Scalar> allValues;
+    view.forEach( [& allValues](const Scalar & val) {
+        if( std::isfinite( val)) {
+            allValues.push_back( val);
+        }
+    });
+
+    // indicate bad clip if no finite numbers were found
+    if( allValues.size() == 0) {
+        return std::make_tuple(
+                    std::numeric_limits<Scalar>::quiet_NaN(),
+                    std::numeric_limits<Scalar>::quiet_NaN());
+    }
+
+    Scalar clip1, clip2;
+    Scalar hist = (1.0 - perc) / 2.0;
+    int x1 = allValues.size() * hist;
+    std::nth_element( allValues.begin(), allValues.begin() + x1, allValues.end());
+    clip1 = allValues[x1];
+    x1 = allValues.size() - 1 - x1;
+    std::nth_element( allValues.begin(), allValues.begin() + x1, allValues.end());
+    clip2 = allValues[x1];
+
+    if(0) { // debug
+        int64_t count = 0;
+        for( auto & x : allValues) {
+            if( x >= clip1 && x <= clip2) { count++ ; }
+        }
+        qDebug() << "autoClip(" << perc << ")=" << count * 100.0 /allValues.size();
+    }
+
+    return std::make_tuple( clip1, clip2);
+}
 
 /// algorithm for converting an instance of image interface to qimage
 class RawView2QImageConverter {
 public:
     typedef RawView2QImageConverter & Me;
+    typedef float Scalar;
+
+    struct CachedImage {
+        QImage img;
+        double clip1, clip2, perc;
+        bool specificToFrame;
+
+        CachedImage( QImage pImg, double c1, double c2, bool spec, double p) {
+            img = pImg; clip1 = c1; clip2 = c2;
+            specificToFrame = spec;
+            perc = p;
+        }
+    };
+
+    RawView2QImageConverter() {
+        // set cache size (in MB)
+        m_cache.setMaxCost( 6 * 1024);
+    }
 
     Me & setView( NdArray::RawViewInterface * rawView) {
         if( ! rawView) {
@@ -31,67 +95,62 @@ public:
         m_rawView = rawView;
         return * this;
     }
-//    Me & setFrame( int frame) {
-//        if( frame > 0) {
-//            int nFrames = 0;
-//            if( m_rawView->dims().size() > 2) {
-//                nFrames = m_rawView->dims()[2];
-//            }
-//            if( frame >= nFrames) {
-//                throw std::runtime_error( "not enough frames in this image");
-//            }
-//        }
-//        return * this;
-//    }
+
+    Me & setFrame( int frame) {
+        if( frame > 0) {
+            int nFrames = 0;
+            if( m_rawView->dims().size() > 2) {
+                nFrames = m_rawView->dims()[2];
+            }
+            if( frame >= nFrames) {
+                throw std::runtime_error( "not enough frames in this image");
+            }
+        }
+        return * this;
+    }
 
     Me & setAutoClip( double val) {
         m_autoClip = val;
         return * this;
     }
 
-    void computeClips() {
-        // read in all values from the view
-        std::vector< float> allValues;
-        NdArray::TypedView<float> tview( m_rawView, false);
-        tview.forEach( [& allValues](const float & val) {
-            if( std::isfinite( val)) {
-                allValues.push_back( val);
+    const QImage & go( int frame, bool recomputeClip = true) {
+
+        // let's see if we have the result in cache
+        CachedImage * img = m_cache[ frame];
+//        img = nullptr;
+//        qDebug() << "Cache check #" << m_frame;
+
+        if( img) {
+            bool match = false;
+            // we have something in cache, let's see if it matches what we want
+            if( recomputeClip) {
+                if( img->specificToFrame && img->perc == m_autoClip) {
+                    match = true;
+                }
             }
-        });
-
-        // indicate bad clip if no finite numbers were found
-        if( allValues.size() == 0) {
-            m_clip1 = 1.0/0.0;
-            return;
+            else {
+                if( img->clip1 == m_clip1 && img->clip2 == m_clip2) {
+                    match = true;
+                }
+            }
+            if( match) {
+                qDebug() << "Cache hit #" << frame;
+                return img->img;
+            }
         }
+        qDebug() << "Cache miss #" << frame;
 
-        double hist = m_autoClip * 100;
-        float clip2;
-        {
-            hist = (1.0 - hist / 100) / 2.0;
-            int x1 = allValues.size() * hist;
-            std::nth_element( allValues.begin(), allValues.begin() + x1, allValues.end());
-            m_clip1 = allValues[x1];
-            x1 = allValues.size() - x1;
-            std::nth_element( allValues.begin(), allValues.begin() + x1, allValues.end());
-            clip2 = allValues[x1];
-        }
-        float clipd = clip2 - m_clip1;
-        m_clipdinv = 1.0 / clipd; // precalculated linear transformation coeff.
-    }
-
-    float m_clip1 = 1.0/0.0, m_clipdinv;
-
-    const QImage & go( bool recomputeClip = true) {
+        NdArray::TypedView<Scalar> view( m_rawView, false);
 
         if( ! std::isfinite(m_clip1) || recomputeClip) {
-            computeClips();
+            std::tie( m_clip1, m_clip2) = computeClips<Scalar>( view, m_autoClip);
         }
 
         int width = m_rawView->dims()[0];
         int height = m_rawView->dims()[1];
 
-        // make sure there is at least 1 non-nan value, if not, return a black image
+        // make sure there is at least 1 finite value, if not, return a black image
         if( ! std::isfinite(m_clip1)) {
             // there were no values.... return a black image
             m_qImage = QImage( width, height, QImage::Format_ARGB32);
@@ -100,14 +159,19 @@ public:
         }
 
         // construct image using clips (clipd, clipinv)
-        static std::vector < QRgb > outBuffer;
-        outBuffer.resize( width * height);
-        QRgb * outPtr = & outBuffer[0];
+        m_qImage = QImage( width, height, QImage::Format_ARGB32);
+        if( m_qImage.bytesPerLine() != width * 4) {
+            throw std::runtime_error( "qimage does not have consecutive memory...");
+        }
+        QRgb * outPtr = reinterpret_cast<QRgb *>( m_qImage.bits());
 
-        NdArray::TypedView<float> tview( m_rawView, false);
-        tview.forEach( [& outPtr, this](const float & ival) {
+        // precalculate linear transformation coeff.
+        float clipdinv = 1.0 / (m_clip2 - m_clip1);
+
+        // apply the clips
+        view.forEach( [& outPtr, & clipdinv, this](const float & ival) {
             if( std::isfinite( ival)) {
-                float val = (ival - m_clip1) * m_clipdinv;
+                float val = (ival - m_clip1) * clipdinv;
                 int gray = val * 255;
                 if( gray > 255) gray = 255;
                 if( gray < 0) gray = 0;
@@ -119,17 +183,48 @@ public:
             outPtr ++;
         });
 
-        // construct the qimage from the temporary buffer
-        m_qImage = QImage( reinterpret_cast<uchar *>( & outBuffer[0]),
-                width, height, QImage::Format_ARGB32);
+//        // construct image using clips (clipd, clipinv)
+//        static std::vector < QRgb > outBuffer;
+//        outBuffer.resize( width * height);
+//        QRgb * outPtr = & outBuffer[0];
+
+//        // precalculate linear transformation coeff.
+//        float clipdinv = 1.0 / (m_clip2 - m_clip1);
+
+//        // apply the clips
+//        view.forEach( [& outPtr, & clipdinv, this](const float & ival) {
+//            if( std::isfinite( ival)) {
+//                float val = (ival - m_clip1) * clipdinv;
+//                int gray = val * 255;
+//                if( gray > 255) gray = 255;
+//                if( gray < 0) gray = 0;
+//                * outPtr = qRgb( gray, gray, gray);
+//            }
+//            else {
+//                * outPtr = qRgb( 255, 0, 0);
+//            }
+//            outPtr ++;
+//        });
+
+//        // construct the qimage from the temporary buffer
+//        m_qImage = QImage( reinterpret_cast<uchar *>( & outBuffer[0]),
+//                width, height, QImage::Format_ARGB32);
+
+        // stick this into the cache so we don't need to recompute it
+        m_cache.insert( frame,
+                        new CachedImage( m_qImage, m_clip1, m_clip2, recomputeClip, m_autoClip),
+                        width * height * 4 / 1024 / 1024);
+
+        qDebug() << "cache total=" << m_cache.totalCost() << "MB";
 
         return m_qImage;
     }
 protected:
     QImage m_qImage;
-    NdArray::RawViewInterface * m_rawView;
+    NdArray::RawViewInterface * m_rawView = nullptr;
     double m_autoClip = 0.95;
-    int m_frame;
+    QCache<int,CachedImage> m_cache;
+    float m_clip1 = 1.0/0.0, m_clip2 = 0.0;
 };
 
 class TestView : public IView
@@ -482,21 +577,15 @@ Viewer::start()
     else {
         m_image = res2.val();
 
-        // convert the loaded image into QImage
-        auto frameSlice = SliceND().next();
-        for( size_t i = 2 ; i < m_image->dims().size() ; i ++) {
-            frameSlice.next().index(0);
-        }
-        NdArray::RawViewInterface * frameView = m_image-> getDataSlice( frameSlice);
-        m_rawView2QImageConverter-> setView( frameView);
-        QImage qimg = m_rawView2QImageConverter-> go();
-        m_currentFrame = 0;
-        delete frameView;
-        qDebug() << "Image loaded" << qimg;
         qDebug() << "Pixel type = " << Image::pixelType2int( res2.val()-> pixelType() );
-
-        testView2 = new TestView2( "view3", QColor( "pink" ), qimg );
+        testView2 = new TestView2( "view3", QColor( "pink" ), QImage(10, 10, QImage::Format_ARGB32) );
         m_connector-> registerView( testView2 );
+
+        // convert the loaded image into QImage
+        m_currentFrame = 0;
+        reloadFrame( true);
+//        delete frameView;
+
     }
 
     if( 0) {
@@ -677,8 +766,8 @@ void Viewer::reloadFrame( bool forceClipRecompute)
     }
     NdArray::RawViewInterface * frameView = m_image-> getDataSlice( frameSlice);
     m_rawView2QImageConverter-> setView( frameView);
-
-    QImage qimg = m_rawView2QImageConverter-> go( m_clipRecompute || forceClipRecompute);
+    QImage qimg = m_rawView2QImageConverter-> go(
+                      m_currentFrame, m_clipRecompute || forceClipRecompute);
     delete frameView;
     testView2->setImage(qimg);
 
