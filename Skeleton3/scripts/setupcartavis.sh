d5ed370f5460744dca9dbee8aa124e8d8a7f8e09
astrilet <astrilet@gmail.com>
2015-05-20 10:12:56 -0700

    Change to compile in release mode & build desktop version only

---
+++ Skeleton3/scripts/setupcartavis.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+####################################################################################################
+# Title       :	setupcartavis.sh
+# Author      :	Alex Strilets, strilets@ualberta.ca
+# Date        :	May 19, 2015
+# Descritpion :	this is shell script to setup catsvis envirotnemt before running cartavis.sh 
+####################################################################################################
+PN=`basename "$0"`
+
+usage () {
+    echo >&2 -e "$PN - shell script setup environment to run CARTAvis viewer\n"
+    exit 1
+}
+
+if [ $# -gt 0 ]; then
+    usage
+fi
+
+dirname=`dirname $0`
+tmp="${dirname#?}"
+
+if [ "${dirname%$tmp}" != "/" ]; then
+dirname=$PWD/$dirname
+fi
+
+#  check for existance of $HOME/.cartavis directory
+if [ ! -d $HOME/.cartavis ]; then
+	echo "creating $HOME/.cartavis directory..."
+	mkdir $HOME/.cartavis
+fi
+
+# check that config.json exists
+if [ ! -f $HOME/.cartavis/config.json ]; then
+	echo "copying config.json file to  $HOME/.cartavis directory..."
+	cp $dirname/config/config.json $HOME/.cartavis
+fi
+
+# check that data directory exists
+if [ ! -d $HOME/data ]; then
+	echo "creating $HOME/data directory..."
+	mkdir $HOME/data
+fi
+
+# check that $HOME/data/ephemerides directory exists
+if [ ! -d $HOME/data/ephemerides ]; then
+	echo "creating link to $HOME/data/ephemerides directory..."
+	ln -s $dirname/data/ephemerides $HOME/data/ephemerides
+fi
+
+# check that $HOME/data/geodetic directory exists
+if [ ! -d $HOME/data/geodetic ]; then
+	echo "creating link to $HOME/data/geodetic directory..."
+	ln -s $dirname/data/geodetic $HOME/data/geodetic
+fi
+
+# check that $HOME/CARTA directory exists
+if [ ! -d $HOME/CARTA ]; then
+	echo "creating $HOME/CARETA directory..."
+	mkdir $HOME/CARTA
+fi
+
+# check that $HOME/CARTA directory exists
+if [ ! -d $HOME/CARTA ]; then
+	echo "creating $HOME/CARETA directory..."
+	mkdir $HOME/CARTA
+fi
+
+# check that $HOME/CARTA/Images directory exists
+if [ ! -d $HOME/CARTA/Images ]; then
+	echo "creating $HOME/CARETA/Images directory..."
+	mkdir $HOME/CARTA/Images
+fi
+
+# check that $HOME/CARTA/snapshots directory exists
+if [ ! -d $HOME/CARTA/snapshots ]; then
+	echo "creating $HOME/CARETA/snapshots directory..."
+	mkdir $HOME/CARTA/snapshots
+fi
+
+# check that sample files exists
+if [ ! -f $HOME/CARTA/Images/555wmos.fits ]; then
+	echo "copying sample images to $HOME/CARTA/Images directory ..."
+	cp $dirname/images/*.fits $HOME/CARTA/Images
+fi
