9028edad4a755c1b66098a502b1fd45fcb7baa60
Alex Strilets <astrilet@gmail.com>
2015-09-29 15:09:40 -0600

    added more tests and updated circle.yml file

--- carta/scripts/runScriptTests.sh
+++ carta/scripts/runScriptTests.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 py.test -v test_cartavis.py::test_getPixelValue
+py.test -v test_cartavis.py::test_getChannelCount
+py.test -v test_cartavis.py::test_getPixelUnits
+py.test -v test_cartavis.py::test_getImageDimensions
+py.test -v test_cartavis.py::test_zoomLevel
+py.test -v test_cartavis.py::test_getCoordinates
+py.test -v test_cartavis.py::test_saveFullImage
+py.test -v test_cartavis.py::test_setColormap
+py.test -v test_cartavis.py::test_setImage
+py.test -v test_cartavis.py::test_invertColormap
+py.test -v test_cartavis.py::test_reverseColormap
+py.test -v test_cartavis.py::test_setColorMix
+py.test -v test_cartavis.py::test_setDataTransform
+py.test -v test_cartavis.py::test_setGamma
+py.test -v test_cartavis.py::test_closeImage
+py.test -v test_cartavis.py::test_getImageNames
