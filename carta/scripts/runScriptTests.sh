caff68f27588ecfb9fdf73fa8a05e77c02e70f48
Alex Strilets <astrilet@gmail.com>
2015-09-30 15:32:21 -0600

    modifed test scripts

--- carta/scripts/runScriptTests.sh
+++ carta/scripts/runScriptTests.sh
@@ -4,19 +4,19 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
-py.test -v test_cartavis.py::test_getPixelValue
-py.test -v test_cartavis.py::test_getChannelCount
-py.test -v test_cartavis.py::test_getPixelUnits
-py.test -v test_cartavis.py::test_getImageDimensions
-py.test -v test_cartavis.py::test_zoomLevel
-py.test -v test_cartavis.py::test_getCoordinates
-py.test -v test_cartavis.py::test_saveFullImage
-py.test -v test_cartavis.py::test_setColormap
-py.test -v test_cartavis.py::test_setImage
-py.test -v test_cartavis.py::test_invertColormap
-py.test -v test_cartavis.py::test_reverseColormap
-py.test -v test_cartavis.py::test_setColorMix
-py.test -v test_cartavis.py::test_setDataTransform
-py.test -v test_cartavis.py::test_setGamma
-py.test -v test_cartavis.py::test_closeImage
-py.test -v test_cartavis.py::test_getImageNames
+try py.test -v test_cartavis.py::test_getPixelValue
+try py.test -v test_cartavis.py::test_getChannelCount
+try py.test -v test_cartavis.py::test_getPixelUnits
+try py.test -v test_cartavis.py::test_getImageDimensions
+try py.test -v test_cartavis.py::test_zoomLevel
+try py.test -v test_cartavis.py::test_getCoordinates
+try py.test -v test_cartavis.py::test_saveFullImage
+try py.test -v test_cartavis.py::test_setColormap
+try py.test -v test_cartavis.py::test_setImage
+try py.test -v test_cartavis.py::test_invertColormap
+try py.test -v test_cartavis.py::test_reverseColormap
+try py.test -v test_cartavis.py::test_setColorMix
+try py.test -v test_cartavis.py::test_setDataTransform
+try py.test -v test_cartavis.py::test_setGamma
+try py.test -v test_cartavis.py::test_closeImage
+try py.test -v test_cartavis.py::test_getImageNames
