0efed7ec2c69ed39d34c95c4423c83041b627efc
astrilet <astrilet@gmail.com>
2016-04-18 09:59:41 -0700

    Fixing CircleCI run

--- carta/scripts/runScriptedClientTests.sh
+++ carta/scripts/runScriptedClientTests.sh
@@ -9,7 +9,7 @@ sleep 10
 
 export LD_LIBRARY_PATH=/opt/casa/lib/libcasa:/opt/casa/lib/libcode/:/opt/casa/otherlib/:/opt/wcslib-5.13/lib/
 cd $HOME/src/build/cpp/desktop
-./desktop --html $HOME/src/CARTAvis/carta/VFS/DesktopDevel/desktop/desktopIndex.html --scriptPort 9999 &
+./desktop --html $HOME/src/CARTAvis/carta/VFS/DesktopDevel/desktop/desktopIndex.html --scriptPort 9999 2>&1 &
 cp /home/developer/src/CARTAvis/carta/scripts/pytest.ini /home/developer/src/CARTAvis/carta/scriptedClient/tests
 cp /home/developer/src/CARTAvis/carta/scripts/runScriptTests.sh /home/developer/src/CARTAvis/carta/scriptedClient/tests
 cd /home/developer/src/CARTAvis/carta/scriptedClient/tests
