a45cb551b7302c9f0052cda87c44de3c4b33fce5
Grimmer Kang <grimmer0125@gmail.com>
2016-12-14 23:27:48 +0800

    Fix the indent error in image.py.

--- carta/scriptedClient/carta/image.py
+++ carta/scriptedClient/carta/image.py
@@ -542,7 +542,7 @@ class Image(CartaView):
         else:
             self.centerOnPixel(float(result[0]), float(result[1]))
         return result
-        
+
     def getPixelValue(self, x, y):
         """
         Get the value of a pixel.
@@ -1337,7 +1337,7 @@ class Image(CartaView):
                                      imageView=self.getId(), value=value)
         return result
 
-    def  setContourLevels(self, contourName, levels):
+    def setContourLevels(self, contourName, levels):
         """
         Update the contour levels within the given contour set.
 
@@ -1446,7 +1446,8 @@ class Image(CartaView):
         list
             Error message if an error occurred; nothing otherwise.
         """
-	    result = self.con.cmdTagList("setCompositionMode", imageView=self.getId(), imageName=imageName)
+
+        result = self.con.cmdTagList("setCompositionMode", imageView=self.getId(), imageName=imageName)
         return result
 
     def isEmpty(self):
