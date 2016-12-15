a4fa92ca5e7a806214d8e1d343d3f776da10131f
Grimmer Kang <grimmer0125@gmail.com>
2016-12-16 01:06:42 +0800

    Modify buildcarta.sh for dockerfile, since we take out library revision from their filename for some libraries.

--- carta/scripts/buildcarta.sh
+++ carta/scripts/buildcarta.sh
@@ -7,6 +7,10 @@ try() { "$@" || die "cannot $*"; }
 HOME=/home/developer
 export PATH=$PATH:$HOME/Qt/5.3/gcc_64/bin
 
+ln -s $HOME/src/CARTAvis-externals/ThirdParty/ast-8.0.2 $HOME/src/CARTAvis-externals/ThirdParty/ast
+ln -s $HOME/src/CARTAvis-externals/ThirdParty/casacore-2.10.2016 $HOME/src/CARTAvis-externals/ThirdParty/casacore 
+ln -s $HOME/src/CARTAvis-externals/ThirdParty/cfitsio-shared $HOME/src/CARTAvis-externals/ThirdParty/cfitsio 
+ln -s $HOME/src/CARTAvis-externals/ThirdParty/imageanalysis-2.10.2016 $HOME/src/CARTAvis-externals/ThirdParty/imageanalysis
 
 cp $HOME/src/CARTAvis/carta/scripts/runScriptedClientTests.sh $HOME
 cp $HOME/src/CARTAvis/carta/scripts/startCARTAServer.sh $HOME
