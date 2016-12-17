4f5edec6e79c7088b0a8b402f33b43edca1f0335
Grimmer Kang <grimmer0125@gmail.com>
2016-12-17 18:11:04 +0800

    Upload a script for linux to run Docker image of carta desktop.

---
+++ carta/scripts/dockerrunlinux.sh
@@ -0,0 +1,5 @@
+# http://stackoverflow.com/questions/59895/getting-the-current-present-working-directory-of-a-bash-script-from-within-the-s
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# $DISPLAY = :0
+docker run -p 9999:9999 -ti --name cartaDesktop -w="/home/developer" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis grimmer0125/cartabuild /bin/bash
