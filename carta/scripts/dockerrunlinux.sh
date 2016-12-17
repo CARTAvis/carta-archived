0e076d1a77fdcf280850ca9aa9bf497fdb1a9ed5
Grimmer Kang <grimmer0125@gmail.com>
2016-12-18 00:11:11 +0800

    fix the but that the previous script will not get the correct path.

--- carta/scripts/dockerrunlinux.sh
+++ carta/scripts/dockerrunlinux.sh
@@ -1,5 +1,6 @@
 # http://stackoverflow.com/questions/59895/getting-the-current-present-working-directory-of-a-bash-script-from-within-the-s
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+DIR=`dirname "$0"`; DIR=`eval "cd \"$DIR\" && pwd"`
 
 # $DISPLAY = :0
 docker run -p 9999:9999 -ti --name cartaDesktop -w="/home/developer" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis grimmer0125/cartabuild /bin/bash
