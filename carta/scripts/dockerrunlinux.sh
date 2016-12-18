4a1dc7ca04c28e7a6a6c9d3ef476d68ccbd08c6e
Grimmer Kang <grimmer0125@gmail.com>
2016-12-19 00:36:58 +0800

    Modify the comments in dockerrunlinux.sh

--- carta/scripts/dockerrunlinux.sh
+++ carta/scripts/dockerrunlinux.sh
@@ -1,8 +1,9 @@
 # http://stackoverflow.com/questions/59895/getting-the-current-present-working-directory-of-a-bash-script-from-within-the-s
+
+# important
 xhost +
+
 DIR=`dirname "$0"`; DIR=`eval "cd \"$DIR\" && pwd"`
 
 # $DISPLAY = :0
-docker run --rm -u 1000 -p 9999:9999 -ti --name cartaDesktop -w="/home/developer" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis grimmer0125/cartabuild:v2 /bin/bash
-
-# not sure which factors let linux ok: xhost, QT_GRAPHICSSYSTEM="native", -u 1000, new docker image 
+docker run --rm -u 1000 -p 9999:9999 -ti --name cartaDesktop -w="/home/developer" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis grimmer0125/cartabuild /bin/bash
