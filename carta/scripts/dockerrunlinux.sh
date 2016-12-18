12e12a5072be53b82aeba20f166ad68146a40e16
Grimmer Kang <grimmer0125@gmail.com>
2016-12-18 18:16:16 +0800

    adjust the script to launch docker version of carta on Linux.

--- carta/scripts/dockerrunlinux.sh
+++ carta/scripts/dockerrunlinux.sh
@@ -1,6 +1,8 @@
 # http://stackoverflow.com/questions/59895/getting-the-current-present-working-directory-of-a-bash-script-from-within-the-s
-
+xhost +
 DIR=`dirname "$0"`; DIR=`eval "cd \"$DIR\" && pwd"`
 
 # $DISPLAY = :0
-docker run -p 9999:9999 -ti --name cartaDesktop -w="/home/developer" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis grimmer0125/cartabuild /bin/bash
+docker run --rm -u 1000 -p 9999:9999 -ti --name cartaDesktop -w="/home/developer" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis grimmer0125/cartabuild:v2 /bin/bash
+
+# not sure which factors let linux ok: xhost, QT_GRAPHICSSYSTEM="native", -u 1000, new docker image 
