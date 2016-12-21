c578776fd524b543d17798a30160d9efff6dd072
Grimmer Kang <grimmer0125@gmail.com>
2016-12-21 12:00:25 +0800

    Add --rm in dockerrunmac.sh when using docker on Mac.

--- carta/scripts/dockerrunmac.sh
+++ carta/scripts/dockerrunmac.sh
@@ -4,4 +4,4 @@ xhost + $ip
 # http://stackoverflow.com/questions/59895/getting-the-current-present-working-directory-of-a-bash-script-from-within-the-s
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run -p 9999:9999 -u 1000 -ti --name cartaDesktop -w="/home/developer" -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis grimmer0125/cartabuild /bin/bash
+docker run --rm -p 9999:9999 -u 1000 -ti --name cartaDesktop -w="/home/developer" -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis grimmer0125/cartabuild /bin/bash
