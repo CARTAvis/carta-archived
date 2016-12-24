1a64a0acb9547b4cd309ec02637906006dc5ba03
Grimmer Kang <grimmer0125@gmail.com>
2016-12-24 14:53:37 +0800

    Set default bash path in docker.

--- carta/scripts/dockerrunserver.sh
+++ carta/scripts/dockerrunserver.sh
@@ -30,11 +30,13 @@ if [ $# -eq 0 ] ; then
     echo "No make arguments supplied"
     docker run --rm -u 1000 -p 8080:8080 -p 9999:9999 -ti --name carta \
         -w="/home/developer" -e DISPLAY=$DISPLAYARG \
+        -e SHELL="/bin/bash" \
         -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis \
         grimmer0125/cartabuild /home/developer/src/CARTAvis/carta/scripts/dockerlaunchServer.sh
 else
     docker run --rm -u 1000 -p 8080:8080 -p 9999:9999 -ti --name carta \
         -w="/home/developer" -e DISPLAY=$DISPLAYARG \
+        -e SHELL="/bin/bash" \
         -v /tmp/.X11-unix:/tmp/.X11-unix -v $DIR/../../:/home/developer/src/CARTAvis \
         -v $1:/scratch/Images
         grimmer0125/cartabuild /home/developer/src/CARTAvis/carta/scripts/dockerlaunchServer.sh
