82a36fecad3b1e79e5540ece2e981edec6f94b46
Grimmer Kang <grimmer0125@gmail.com>
2017-02-17 23:14:27 +0800

    Add some notes needed to execute before running carta.

--- carta/scripts/generateUIfile.sh
+++ carta/scripts/generateUIfile.sh
@@ -1,3 +1,65 @@
-# modify common_config.pri
+##  modify common_config.pri
+#gcc481 {
+        #    COMPILER = g++-4.8
 
-# build UI files
+## build UI files
+# cd $CARTAWORKHOME/CARTAvis/carta/html5/common/skel
+# ./generate.py > /dev/null
+
+## to build carta, Qt (webkit) needs some .so 
+yum -y install gstreamer-devel
+yum -y install gstreamer-plugins-base
+
+yum -y install python-devel # for Python.h
+
+## setupt geodetic, ephemerides
+
+##  copy fits file
+# mkdir -p ~/CARTA/Images, 
+# mkdir -p /scratch/Images
+# cp aJ.fits /scratch/Images/
+# cp aJ.fits ~/CARTA/Images/
+##
+
+## setup ~/.cartavis/config.json/config.json
+# {
+# "_comment" : "List of plugin directories",
+# "pluginDirs": [
+# "$(APPDIR)/../plugins",
+# "$(APPDIR)/../../../../plugins",
+# "$(HOME)/.cartavis/plugins"
+# ],
+# "disabledPlugins" : ["casaCore-2.0.1"],
+# "plugins": {
+#     "PCacheSqlite3" : {
+#         "dbPath": "/tmp/pcache.sqlite"
+#     }
+# }
+
+# }
+
+## before running 
+#export LD_LIBRARY_PATH=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/casa/trunk/linux/lib:${LD_LIBRARY_PATH}
+
+## when running carta, loading ImageStatistics fail
+# "Loading plugin RegionCASA[c++]"
+# Trying to load native plugin "RegionCASA"
+#   - heuristics to load libraries: 0
+# Raw plugin loaded.
+# Carta plugin loaded.
+# Calling plugin's initialize
+# Calling plugin's getInitialHookList
+# Plugin initialized
+# "Loading plugin ImageStatistics[c++]"
+# Trying to load native plugin "ImageStatistics"
+#   - heuristics to load libraries: 0
+# QPluginLoader error =  "Cannot load library /root/src/CARTAvis/build/cpp/plugins/ImageStatistics/libplugin.so: (libplugin.so: cannot open shared object file: No such file or directory)"
+# QLibrary error: "Cannot load library /root/src/CARTAvis/build/cpp/plugins/ImageStatistics/libplugin.so: (libplugin.so: cannot open shared object file: No such file or directory)"
+# "Failed to load plugin ImageStatistics[c++]"
+# ...reasons:  "QPluginLoader error: Cannot load library /root/src/CARTAvis/build/cpp/plugins/ImageStatistics/libplugin.so: (libplugin.so: cannot open shared object file: No such file or directory)
+# QLibrary error:Cannot load library /root/src/CARTAvis/build/cpp/plugins/ImageStatistics/libplugin.so: (libplugin.so: cannot open shared object file: No such file or directory)"
+# "Loading plugin DevIntegration[c++]"
+# Trying to load native plugin "DevIntegration"
+#   - heuristics to load libraries: 0
+# Raw plugin loaded.
+# Carta plugin loaded.
