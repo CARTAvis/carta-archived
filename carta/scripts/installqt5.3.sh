3db955f098998fea7a26c3b3e3d2d50015dbc7dc
Grimmer <grimmer0125@gmail.com>
2017-02-18 20:07:11 +0800

    Update installqt5.3.sh

--- carta/scripts/installqt5.3.sh
+++ carta/scripts/installqt5.3.sh
@@ -5,6 +5,7 @@
 # cd ~/src
 
 CARTAWORKHOME=`pwd`
+QTINSTALLER=qt-unified-linux-x64-2.0.5-online.run
 
 # sudo yum -y install wget
 sudo yum -y install mesa-libGL-devel
@@ -13,11 +14,12 @@ sudo yum -y install mesa-libGL-devel
 # possible to use sudo yum to install qt 5.3.2 ? ref: qt5-qtbase-5.2.0-4.fc20.x86_64?
 mkdir -p ~/download
 cd ~/download
-## or http://download.qt.io/archive/qt/5.8/5.8.0/qt-opensource-linux-x64-5.8.0.run
-wget http://download.qt.io/archive/qt/5.3/5.3.2/qt-opensource-linux-x64-5.3.2.run
-chmod 755 qt-opensource-linux-x64-5.3.2.run
+## wget http://download.qt.io/archive/qt/5.8/5.8.0/qt-opensource-linux-x64-5.8.0.run
+## wget http://download.qt.io/archive/qt/5.3/5.3.2/qt-opensource-linux-x64-5.3.2.run
+wget http://ftp.jaist.ac.jp/pub/qtproject/archive/online_installers/2.0/$QTINSTALLER
+chmod 755 $QTINSTALLER
 # http://doc.qt.io/qt-5/linux.html
-mkdir -p $CARTAWORKHOME/CARTAvis-externals/ThirdParty/Qt5.3.2 ## include qt creaetor
-./qt-opensource-linux-x64-5.3.2.run ## will prompt UI to install, but font is missing
+mkdir -p $CARTAWORKHOME/CARTAvis-externals/ThirdParty/Qt ## include qt creaetor
+./$QTINSTALLER ## will prompt UI to install, but font is missing
 # choose to install in  ~/src/CARTAvis-externals/ThirdParty/Qt5.3.2/,
 # can change to use default location, ~/Qt/?
