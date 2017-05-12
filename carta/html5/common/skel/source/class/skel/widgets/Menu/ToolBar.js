159b1debf4a6cc60a6a22f6519e0d5969f13baaa
Grimmer <grimmer0125@gmail.com>
2017-05-12 12:18:24 +0800

    Show “clipping:” & “Region:” in toolbar.

--- carta/html5/common/skel/source/class/skel/widgets/Menu/ToolBar.js
+++ carta/html5/common/skel/source/class/skel/widgets/Menu/ToolBar.js
@@ -22,7 +22,7 @@ qx.Class.define("skel.widgets.Menu.ToolBar", {
     },
 
     members : {
-        
+
 
 
         /**
@@ -30,38 +30,99 @@ qx.Class.define("skel.widgets.Menu.ToolBar", {
          * @param cmds {Array} the list of available commands.
          */
         _buildToolBar : function ( cmds ){
+            // init時被called到, 還有內部recursive
+            // 把qx.ui.menu.CheckBox加進去, this.add or
+            // qx.ui.menu.Button or
+            // 一個 array, of new qx.ui.menu.RadioButton or qx.ui.toolbar.RadioButton 中途它們可能被加進去 qx.ui.form.RadioGroup !!!
+            console.log("grimmer toolbar-_buildToolBar", cmds);
             if ( cmds.isVisibleToolbar() ){
                 if ( cmds.getType() === skel.Command.Command.TYPE_COMPOSITE ){
+                    console.log("grimmer toolbar-composite");
                     var vals = cmds.getValue();
                     for ( var i = 0; i < vals.length; i++ ){
                         this._buildToolBar( vals[i]);
                     }
+                    console.log("grimmer toolbar-composite2");
                 }
                 else if ( cmds.getType() == skel.Command.Command.TYPE_GROUP ){
-                    this._makeTool( cmds );
+                    console.log("grimmer toolbar-group");
+                    this._makeTool( cmds ); //可能會用到 qx.ui.form.RadioGroup?
                 }
                 else if ( cmds.getType() == skel.Command.Command.TYPE_BOOL ){
+                    // console.log("grimmer toolbar-bool");
                     var check = skel.widgets.Util.makeCheck( cmds, null, true );
                     this.add( check );
                 }
                 else if ( cmds.getType() == skel.Command.Command.TYPE_BUTTON ){
+                    // console.log("grimmer toolbar-button");
                     var button = skel.widgets.Util.makeButton( cmds, null, true, true );
                     this.add( button );
                 }
                 else {
                     console.log( "Tool bar unsupported command type="+ cmds.getType() );
                 }
+            } else {
+                console.log("grimmer toolbar-invisile");
             }
         },
-        
+
+        _buildToolBar2 : function ( cmds ){
+            // init時被called到, 還有內部recursive
+            // 把qx.ui.menu.CheckBox加進去, this.add or
+            // qx.ui.menu.Button or
+            // 一個 array, of new qx.ui.menu.RadioButton or qx.ui.toolbar.RadioButton 中途它們可能被加進去 qx.ui.form.RadioGroup !!!
+            console.log("grimmer toolbar2-_buildToolBar", cmds);
+            if ( cmds.isVisibleToolbar() ){
+                if ( cmds.getType() === skel.Command.Command.TYPE_COMPOSITE ){
+                    console.log("grimmer toolbar2-composite");
+                    var vals = cmds.getValue();
+                    for ( var i = 0; i < vals.length; i++ ){
+                        this._buildToolBar2( vals[i]);
+                    }
+                    console.log("grimmer toolbar2-composite2");
+                }
+                else if ( cmds.getType() == skel.Command.Command.TYPE_GROUP ){
+                    console.log("grimmer toolbar2-group");
+                    this._makeTool( cmds ); //可能會用到 qx.ui.form.RadioGroup?
+                }
+                else if ( cmds.getType() == skel.Command.Command.TYPE_BOOL ){
+                    // console.log("grimmer toolbar-bool");
+                    var check = skel.widgets.Util.makeCheck( cmds, null, true );
+                    this.add( check );
+                }
+                else if ( cmds.getType() == skel.Command.Command.TYPE_BUTTON ){
+                    // console.log("grimmer toolbar-button");
+                    var button = skel.widgets.Util.makeButton( cmds, null, true, true );
+                    this.add( button );
+                }
+                else {
+                    console.log( "Tool bar unsupported command type="+ cmds.getType() );
+                }
+            } else {
+                console.log("grimmer toolbar2-invisile");
+            }
+        },
+
         /**
          * Initialize a tool bar containing 'quick' access buttons.
          */
         _init : function() {
+
+            // CAS-9814 Retrieve initial layout type to enable check status of default layout.
+            // DisplayMain.js
+            //                     var layoutType = layout.layoutType;
+                    // if(layoutType) {
+                    //     skel.Command.Layout.CommandLayout.getInstance().setCheckedType(layoutType);
+                    // }
+
+            //skel.Command.Clip.CommandClipValues?
+            // 0.99
             var cmds = skel.Command.CommandAll.getInstance();
+            console.log("grimmer toolbar2-init:", cmds);
+
             this._resetToolbar( cmds );
         },
-        
+
         /**
          * Initialize a context menu.
          */
@@ -81,7 +142,7 @@ qx.Class.define("skel.widgets.Menu.ToolBar", {
             contextMenu.add( removeButton );
             this.setContextMenu(contextMenu);
         },
-        
+
         /**
          * Initialize the list of messages to listen to.
          */
@@ -93,9 +154,9 @@ qx.Class.define("skel.widgets.Menu.ToolBar", {
             qx.event.message.Bus.subscribe( "commandVisibilityToolChanged", function( message){
                 this._init();
             }, this );
-            
+
         },
-        
+
         /**
          * Tell a command to perform its action.
          * @param lookup {String} an identifier for a command.
@@ -107,27 +168,59 @@ qx.Class.define("skel.widgets.Menu.ToolBar", {
             var cmd = cmdFactory.getCommand( lookup);
             cmd.doAction( val, undo );
         },
-        
-       
-        
+
+
+
         /**
          * Make a widget for the tool bar out of the command.
          * @param cmd {skel.Command.Command}.
          */
         _makeTool : function( cmd ){
+
+            //百分百的每一的都有完整的% array, m_cmds, 來了5,6個
+            console.log("grimmer toolbar3-_makeTool:", cmd);
+            console.log("grimmer toolbar3-2:", cmd.length);
+
             if ( cmd.isVisibleToolbar()){
                 var cmdType = cmd.getType();
                 if ( cmdType === skel.Command.Command.TYPE_GROUP){
+
+                    if(cmd.getLabel()=="Clips") {
+                        var fileButton = new qx.ui.toolbar.RadioButton("Clipping:");
+                        fileButton.setEnabled(false);
+                        this.add(fileButton);
+                    } else if (cmd.getLabel()=="Draw") {
+                        var fileButton = new qx.ui.toolbar.RadioButton("Region:");
+                        fileButton.setEnabled(false);
+                        this.add(fileButton);
+                    }
+
+                    console.log("grimmer toolbar-makeTool-group:", cmd.getLabel());
+                    //CommandClipValues: "Clips"
+                    // 只有 clip & region
+                    //  :"Draw" or 各種region type?
+
                     var radios = skel.widgets.Util.makeRadioGroup( cmd, true );
                     for ( var i = 0; i < radios.length; i++ ){
-                        this.add( radios[i] );
+                        this.add( radios[i] ); //!!!!!!!!!!!!
                     }
+
+                    //var fileButton = new qx.ui.toolbar.MenuButton("File");
+
+                    // var fileButton = new qx.ui.menu.RadioButton(); not work
+
+
+                    console.log("grimmer toolbar-makeTool-group2:");
                 }
                 else if ( cmdType === skel.Command.Command.TYPE_BOOL){
+                    // console.log("grimmer toolbar3-_bool:");
+
                     var check = skel.widgets.Util.makeCheck( cmd, null, true );
                     this.add( check );
                 }
                 else if ( cmdType == skel.Command.Command.TYPE_BUTTON ){
+                    // console.log("grimmer toolbar3-_button:");
+
                     var button = skel.widgets.Util.makeButton( cmd, null, true, true );
                     this.add( button );
                 }
@@ -136,22 +229,23 @@ qx.Class.define("skel.widgets.Menu.ToolBar", {
                 }
             }
             else {
-                console.log( "Cmd was not visible on toolbar "+cmd );
+                console.log( "grimmer toolbar Cmd was not visible on toolbar "+cmd );
             }
         },
-        
-        
+
+
         /**
          * Erase the current tool bar and reset with the new list of commands.
          * @param cmds {Array} the new list of commands.
          */
         _resetToolbar : function( cmds ){
+            // console.log("grimmer toolbar4:", cmds.length);
             this.removeAll();
             this._buildToolBar( cmds );
             this.addSpacer();
         }
     }
-    
-    
+
+
 
 });
