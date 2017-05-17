86171432e30e99fcc09c3b9ab958858d388959b6
Grimmer <grimmer0125@gmail.com>
2017-05-17 14:30:30 +0800

    1. Organize build scripts. 2. Remove the installation of some libs for casa. 3. Use git to download the patch for casa/code.

--- carta/scripts/buildcasa.sh
+++ carta/scripts/buildcasa.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-export CARTAWORKHOME=`pwd`
+
+## TODO: remove installinging
+## java/jre, pgplot, dbus, qt4.
+## qwt has been removed
+
+if [ -z ${cartawork+x} ]; then
+	export cartawork=`pwd`
+else
+fi
 
 isCentOS=true
 if grep -q CentOS /etc/os-release; then
@@ -13,11 +21,10 @@ TARGETOS=linux
 
 if [ "$(uname)" == "Darwin" ]; then
     ## should do https://github.com/CARTAvis/carta/wiki/Install-Third-Party-For-CARTA-CASA-On-Mac first
-    ## its qmake (homebrew) is in /usr/local/Cellar/qt/4.8.7_3/bin
 
     TARGETOS=darwin
 
-    ## in this path, in include "dbuspp-xml2cpp" which will affect code's cmake:DBUS_FLAVOR_dbuspp-xml2cpp_EXECUTABLE
+    ## in this path, it includes "dbuspp-xml2cpp" which will affect code's cmake:DBUS_FLAVOR_dbuspp-xml2cpp_EXECUTABLE
     ## without this, casa-code's CMakeLists' requirement becomes dbus-c++ from dbus-cpp
     export PATH=/opt/casa/02/bin:$PATH
 
@@ -29,61 +36,25 @@ name=CASA RPMs for RedHat Enterprise Linux 7 (x86_64)
 baseurl=http://svn.cv.nrao.edu/casa/repo/el7/x86_64
 gpgkey=http://svn.cv.nrao.edu/casa/RPM-GPG-KEY-casa http://www.jpackage.org/jpackage.asc http://svn.cv.nrao.edu/casa/repo/el7/RPM-GPG-KEY-redhat-release http://svn.cv.nrao.edu/casa/repo/el7/RPM-GPG-KEY-EPEL
 EOF
-
-
-    # for building Qt 4.8.5,
-    # On Grimmer's docker -centos 7.3 , only when buidling Carta by Qt-5.3.2, we need gstreamer-devel, gstreamer-plugins-base
-    # But on Chia-Jung Hsu's pc - centos 7.3, building Qt 4.8.5 needs pkgconfig(gstreamer-app-0.10) which install gstreamer-plugins-base-devel ,
-    #    also gstreamer-devel, gstreamer-plugins-base,  gstreamer
-    # Qt's QtWebkit may use gstreamer libs
-    sudo yum -y install 'pkgconfig(gstreamer-app-0.10)'
-
     ##### CentOS 7
     ## https://safe.nrao.edu/wiki/bin/view/Software/CASA/CartaBuildInstructionsForEL7
     ## To do: should spend time to minimalize them,
     ## also tell them which part is for casacore, which is for casa(code)
     ## only support CentOS 7 ??
-    # sudo yum -y install devtoolset*
+    # sudo yum -y install devtoolset* (includes devtoolset-3 gcc-4.9.2, fortran, glib, python)
 
-    #   devtoolset-3-gcc.x86_64 0:4.9.2-6.2.el7
-    #   devtoolset-3-gcc-c++.x86_64 0:4.9.2-6.2.el7
-    #   devtoolset-3-gcc-gfortran.x86_64 0:4.9.2-6.2.el7
-    ## not sure if other libs are needed or not. seems not
-    # Installed:
-    #   devtoolset-3-binutils.x86_64 0:2.24-18.el7                          devtoolset-3-dwz.x86_64 0:0.11-1.1.el7                            devtoolset-3-elfutils.x86_64 0:0.161-1.el7
-    #   devtoolset-3-elfutils-libelf.x86_64 0:0.161-1.el7                   devtoolset-3-elfutils-libs.x86_64 0:0.161-1.el7                   devtoolset-3-gcc.x86_64 0:4.9.2-6.2.el7
-    #   devtoolset-3-gcc-c++.x86_64 0:4.9.2-6.2.el7                         devtoolset-3-gcc-gfortran.x86_64 0:4.9.2-6.2.el7                  devtoolset-3-gdb.x86_64 0:7.8.2-38.el7
-    #   devtoolset-3-libquadmath-devel.x86_64 0:4.9.2-6.2.el7               devtoolset-3-libstdc++-devel.x86_64 0:4.9.2-6.2.el7               devtoolset-3-ltrace.x86_64 0:0.7.91-9.el7
-    #   devtoolset-3-memstomp.x86_64 0:0.1.5-3.el7                          devtoolset-3-runtime.x86_64 0:3.1-12.el7                          devtoolset-3-strace.x86_64 0:4.8-8.el7
-    #   devtoolset-3-toolchain.x86_64 0:3.1-12.el7
-    #
-    # Dependency Installed:
-    #   audit-libs-python.x86_64 0:2.6.5-3.el7          checkpolicy.x86_64 0:2.5-4.el7                    glibc-devel.x86_64 0:2.17-157.el7_3.1          glibc-headers.x86_64 0:2.17-157.el7_3.1
-    #   kernel-headers.x86_64 0:3.10.0-514.6.1.el7      libcgroup.x86_64 0:0.41-11.el7                    libgomp.x86_64 0:4.8.5-11.el7                  libmpc.x86_64 0:1.0.1-3.el7
-    #   libselinux-python.x86_64 0:2.5-6.el7            libselinux-utils.x86_64 0:2.5-6.el7               libsemanage-python.x86_64 0:2.5-5.1.el7_3      mpfr.x86_64 0:3.1.1-4.el7
-    #   policycoreutils.x86_64 0:2.5-11.el7_3           policycoreutils-python.x86_64 0:2.5-11.el7_3      python-IPy.noarch 0:0.75-6.el7                 scl-utils.x86_64 0:20130529-17.el7_1
-    #   setools-libs.x86_64 0:3.3.8-1.1.el7
-    #
-    # Dependency Updated:
-    #   libsemanage.x86_64 0:2.5-5.1.el7_3
-    ####
-    # Xerces-C++ is a validating XML parser
-
-    ## need to check if we really need this to install casa01-mpi4py, casa01-openmpi
+    ## TODO: need to check if we really need this to install casa01-mpi4py, casa01-openmpi
     ## https://safe.nrao.edu/wiki/bin/view/Software/CASA/CartaBuildInstructionsForEL7
 
-    ##  casa01-python-tools seems to have python-numpy, boost-python
+    ## casa01-python-tools seems to have python-numpy, boost-python
 
     sudo yum -y install python-devel boost-python
     # casa01-mpi4py.x86_64 casa01-openmpi.x86_64 \
     # casa01-python.x86_64 casa01-python-devel.x86_64, casa01-python-tools.x86_64 \ (2.7.5)
-    sudo yum -y install lapack-devel xerces-c-devel
+    sudo yum -y install lapack-devel xerces-c-devel #Xerces-C++ is a validating XML parser
 
-    # sudo yum -y install wcslib wcslib-devel
-
-    ## casacore needs. not sure if code needs or not
-    ## casacore refernece: https://github.com/casacore/casacore
-    ## the above link shows. (optional) yum -y install ncurses ncurses-devel, but it is default when building on Ubuntu?
+    ## casacore/code needs boost. casacore refernece: https://github.com/casacore/casacore
+    ## the above link shows. (optional) yum -y install ncurses ncurses-devel
     # sudo yum -y install gcc-gfortran ## 4.8.5, ast also needs this
     sudo yum -y install boost
     sudo yum -y install boost-devel
@@ -177,62 +148,55 @@ else
 
     ##### Qt 4.8.5 way1:  Build (slow)
     #wget https://download.qt.io/archive/qt/4.8/4.8.5/qt-everywhere-opensource-src-4.8.5.zip
-    #unzip -a qt-everywhere-opensource-src-4.8.5.zip # -d $CARTAWORKHOME/CARTAvis-externals/ThirdParty/Qt4.8.5
-    ## cd $CARTAWORKHOME/CARTAvis-externals/ThirdParty/Qt4.8.5/qt-everywhere-opensource-src-4.8.5
+    #unzip -a qt-everywhere-opensource-src-4.8.5.zip # -d $cartawork/CARTAvis-externals/ThirdParty/Qt4.8.5
+    ## cd $cartawork/CARTAvis-externals/ThirdParty/Qt4.8.5/qt-everywhere-opensource-src-4.8.5
     #cd qt-everywhere-opensource-src-4.8.5
     #printf 'yes\n' | ./configure -v -opensource -dbus-linked
     #make
     #make install
     #export PATH=/usr/local/Trolltech/Qt-4.8.5/bin:$PATH
     #######
-
-    # ./configure --prefix $CARTAWORKHOME/CARTAvis-externals/ThirdParty/Qt4.8.5 -> fail
+    ## ./configure --prefix $cartawork/CARTAvis-externals/ThirdParty/Qt4.8.5 -> fail, not know why
     #./configure # some interactive questioin. "o", "yes" !!
 
-    ##### TODO: way2, to install Qt 4.8.7, use qmake or qmake-qt4. Not use this way to test all.
+    ## TODO: way2, to install Qt 4.8.7, use qmake or qmake-qt4. Not use this way to test all.
     sudo apt-get install libqt4-dev
 fi
 
-# can try to use tools to answer stdin questions automatically
-# http://askubuntu.com/questions/338857/automatically-enter-input-in-command-line
-# http://stackoverflow.com/questions/14392525/passing-arguments-to-an-interactive-program-non-interactively
-# http://stackoverflow.com/questions/4857702/how-to-provide-password-to-a-command-that-prompts-for-one-in-bash
-# or use sudo yum to insatll ? qt-4.8.6-30.fc20.x86_64
+cd $cartawork/CARTAvis-externals/ThirdParty
 
-cd $CARTAWORKHOME/CARTAvis-externals/ThirdParty
+### Qwt: for casa-submodue-code, by using Qt4.8.5
+# curl -O -L http://downloads.sourceforge.net/project/qwt/qwt/6.1.0/qwt-6.1.0.tar.bz2
+# tar xvfj qwt-6.1.0.tar.bz2 && mv qwt-6.1.0 qwt-6.1.0-src
+# cd qwt-6.1.0-src # can use qwt 6.1.3 Pavol uses
+# # for unix part
+# if [ "$(uname)" == "Darwin" ]; then
+#     perl -i -pe 's/.*/ QWT_INSTALL_PREFIX    = $ENV{cartawork}\/CARTAvis-externals\/ThirdParty\/qwt-6.1.0\/ / if $.==22' qwtconfig.pri
+# else
+#     sed -i "22,22c QWT_INSTALL_PREFIX    = $cartawork/CARTAvis-externals/ThirdParty/qwt-6.1.0" qwtconfig.pri
+# fi
+# qmake qwt.pro
+# make && make install
+# # export PATH=$cartawork/CARTAvis-externals/ThirdParty/qwt-6.1.0/include:$PATH
+# # export PATH=$cartawork/CARTAvis-externals/ThirdParty/qwt-6.1.0/lib:$PATH
+# cd ..
+# if [ "$(uname)" == "Darwin" ]; then
+# 	cd qwt-6.1.0
+#     mkdir include
+# 	ln -s ../lib/qwt.framework/Versions/6/Headers include/qwt
+#     ln -s ../lib/qwt.framework/Versions/6/qwt ./lib/qwt
+# 	cd ..
+# fi
 
-## for building qwt for casa-submodue-code, by using Qt4.8.5
-curl -O -L http://downloads.sourceforge.net/project/qwt/qwt/6.1.0/qwt-6.1.0.tar.bz2
-tar xvfj qwt-6.1.0.tar.bz2 && mv qwt-6.1.0 qwt-6.1.0-src
-cd qwt-6.1.0-src # can use qwt 6.1.3 Pavol uses
-# for unix part
-if [ "$(uname)" == "Darwin" ]; then
-    perl -i -pe 's/.*/ QWT_INSTALL_PREFIX    = $ENV{CARTAWORKHOME}\/CARTAvis-externals\/ThirdParty\/qwt-6.1.0\/ / if $.==22' qwtconfig.pri
-else
-    sed -i "22,22c QWT_INSTALL_PREFIX    = $CARTAWORKHOME/CARTAvis-externals/ThirdParty/qwt-6.1.0" qwtconfig.pri
-fi
-qmake qwt.pro
-make && make install
-# export PATH=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/qwt-6.1.0/include:$PATH
-# export PATH=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/qwt-6.1.0/lib:$PATH
-cd ..
-if [ "$(uname)" == "Darwin" ]; then
-	cd qwt-6.1.0
-    mkdir include
-	ln -s ../lib/qwt.framework/Versions/6/Headers include/qwt
-    ln -s ../lib/qwt.framework/Versions/6/qwt ./lib/qwt
-	cd ..
-fi
-
-## cascore and code
+#### cascore and code
 # in the other instruction to build carta + casa, usually
 # casa
-# cartabuild(CARTAWORKHOME) / CARTAvis(carta, git root folder)
-# but we put casa inside $CARTAWORKHOME/CARTAvis-externals/ThirdParty
+# cartabuild(cartawork) / CARTAvis(carta, git root folder)
+# but we put casa inside $cartawork/CARTAvis-externals/ThirdParty
 mkdir casa
 cd casa
 
-### old svn way
+### old svn way:
 # it seems that its svn external link, casa submodule - casacore will checkout its latest one
 # svn co --ignore-externals -r 38314 https://svn.cv.nrao.edu/svn/casa/trunk
 # cd trunk
@@ -242,9 +206,7 @@ cd casa
 # # no need to rename for git way
 # svn co -r 105507 https://github.com/casacore/casacore/trunk
 # mv trunk casacore
-###
-
-### new total git way + only clone casacore without asap part
+### new git way: total git way + only clone casacore without asap part
 git clone https://open-bitbucket.nrao.edu/scm/casa/casa.git trunk
 cd trunk
 git checkout 77a3c0170c895142883dc1b69c4996f430c9e8ec ## = 5.0.0-mas-193, 20170506
@@ -256,19 +218,16 @@ git submodule update --init casacore
 cd casacore
 mkdir build && cd build
 
-# https://sites.google.com/a/asiaa.sinica.edu.tw/acdc/bui/centos7-2,
-# maybe this is due to the above reason, required by latest versoin of casacore
-# export PATH=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/cfitsio/include:$PATH \
-# export PATH=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/cfitsio/lib:$PATH
-
 ###  two more official build refernece
 # https://github.com/casacore/casacore
 # https://github.com/casacore/casacore/wiki/CmakeInstructions
+# https://safe.nrao.edu/wiki/bin/view/Software/CASA/CasaCMakeFlagsOSX1010
+# https://safe.nrao.edu/wiki/bin/view/Software/CASA/CartaBuildInstructionsForUbuntu
 ###
 
 ## it is better to rm -rf * in build folder if rebuild manually + dependency changes
 ## can use your own compiler and gfortan here
-## use $CARTAWORKHOME may be better for -DCMAKE_INSTALL_PREFIX=../../linux
+## use $cartawork may be better for -DCMAKE_INSTALL_PREFIX=../../linux
 ## After checking, /usr/lib64/casa/01 does not exist but build OK
 
 if [ "$(uname)" == "Darwin" ]; then
@@ -276,8 +235,8 @@ if [ "$(uname)" == "Darwin" ]; then
     -DUseCasacoreNamespace=1 \
     -DCMAKE_Fortran_COMPILER=/usr/local/Cellar/gcc/6.3.0_1/bin/gfortran-6 \
     -DPYTHON_LIBRARY=/System/Library/Frameworks/Python.framework/Versions/2.7/Python \
-    -DWCSLIB_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/wcslib \
-    -DCFITSIO_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/cfitsio \
+    -DWCSLIB_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/wcslib \
+    -DCFITSIO_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/cfitsio \
     -DBOOST_ROOT=/usr/local/Cellar/boost/1.63.0 \
     -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
     -DCMAKE_C_COMPILER=/usr/bin/clang \
@@ -289,8 +248,8 @@ elif [ "$isCentOS" = true ] ; then
     -DCMAKE_INSTALL_PREFIX=../../$TARGETOS -DBUILD_PYTHON=1 \
     -DPYTHON_INCLUDE_DIR=/usr/include/python2.7 \
     -DPYTHON_LIBRARY=/usr/lib64/libpython2.7.so \
-    -DWCSLIB_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/wcslib \
-    -DCFITSIO_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/cfitsio \
+    -DWCSLIB_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/wcslib \
+    -DCFITSIO_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/cfitsio \
     -DCMAKE_BUILD_TYPE=Release \
     -DCXX11=1 ..
     # -DPYTHON_LIBRARY=/opt/casa/01/lib/libpython2.7.so \ ??
@@ -307,26 +266,15 @@ else
     -DCMAKE_BUILD_TYPE=Release -DCXX11=1 ..
 fi
 
-### Mac part:
-# -DCMAKE_Fortran_COMPILER=/opt/casa/02/bin/gfortran-mp-5 \ ?
-# -DPYTHON_LIBRARY=/opt/casa/02/lib/libpython2.7.dylib \ ?
-## DUseCrashReporter seems to be not used by casacore
-# cmake -DBoost_NO_BOOST_CMAKE=1 -DCASA_BUILD=1 \
-# -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
-# -DCMAKE_C_COMPILER=/usr/bin/clang \
-# -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF \
-# -DCMAKE_INSTALL_PREFIX=../../darwin -DBUILD_PYTHON=1 -DCXX11=1 ..
-###
-
-# it may build fail due to no official parallel build support of casa
+# it may build fail due to no official parallel build support of casa, switch to use "make" when fail
 make -j2
 make install
 
 ############################ casa-submodule: code/imageanalysis
+
 cd ../../code
 
-
-### old
+### old:
 # # Apply https://safe.nrao.edu/wiki/pub/Software/CASA/CartaBuildInstructionsForUbuntu/imageanalysisonubuntu.diff
 # # to Code to build imageanalysis only. Reduce build time a lot !!!!!
 # curl -O http://www.asiaa.sinica.edu.tw/~tckang/casa/casacodereduce1.diff
@@ -351,16 +299,14 @@ cd ../../code
 #   sed -i 's/.*casa_add_module( parallel/#&/' CMakeLists.txt
 #   sed -i 's/.*casa_add_module( casadbus/#&/' CMakeLists.txt
 # fi
-###
-
-### new for new git way/repo, will improve later. 201705
-curl -O http://www.asiaa.sinica.edu.tw/~tckang/casa/casacodereduce2.diff
-git apply casacodereduce2.diff
+### new:  for new git way/repo, will improve later. 201705
+curl -O https://raw.githubusercontent.com/grimmer0125/tmp/master/casacodereduce3.diff
+git apply casacodereduce3.diff
 ###
 
 mkdir build && cd build
 
-# -DEXTRA_C_FLAGS=-DPG_PPU -I/opt/casa/02/include/wcslib
+# TODO: check what this means? -DEXTRA_C_FLAGS=-DPG_PPU -I/opt/casa/02/include/wcslib
 
 ## it is better to rm -rf * in build folder if rebuild manually + dependency changes
 ## DSKIP_PGPLOT is used by display, qtviewer/guitools?, or some imageanalysis/test
@@ -368,8 +314,8 @@ if [ "$(uname)" == "Darwin" ]; then
     cmake -DUseCrashReporter=0 -DBoost_NO_BOOST_CMAKE=1 \
     -DUseCasacoreNamespace=1 \
     -Darch=$TARGETOS -DCMAKE_BUILD_TYPE=Release -DCXX11=1 \
-    -DWCSLIB_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/wcslib \
-    -DCFITSIO_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/cfitsio \
+    -DWCSLIB_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/wcslib \
+    -DCFITSIO_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/cfitsio \
     -DREADLINE_ROOT_DIR=/usr/local/opt/readline \
     -DPGPLOT_ROOT_DIR=/usr/local/opt/pgplot \
     -DBOOST_ROOT=/usr/local/Cellar/boost/1.63.0 \
@@ -377,9 +323,9 @@ if [ "$(uname)" == "Darwin" ]; then
     -DPGPLOT_LIBRARIES=/usr/local/opt/pgplot/lib \
     -DSKIP_PGPLOT=1 \
     -DLIBXML2_ROOT_DIR=/usr/local/opt/libxml2 \
-    -DQWT_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/qwt-6.1.0 \
+    -DQWT_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/qwt-6.1.0 \
     -DXERCES_ROOT_DIR=/usr/local/opt/xerces-c \
-    -DRPFITS_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/rpfits \
+    -DRPFITS_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/rpfits \
     -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
     -DCMAKE_C_COMPILER=/usr/bin/clang \
     -DLLVMCOMPILER=1 \
@@ -387,9 +333,9 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$isCentOS" = true ] ; then
     cmake -DUseCrashReporter=0 -DBoost_NO_BOOST_CMAKE=1 -DEXTRA_C_FLAGS=-DPG_PPU \
     -DUseCasacoreNamespace=1 \
-    -DWCSLIB_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/wcslib \
-    -DCFITSIO_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/cfitsio \
-    -DQWT_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/qwt-6.1.0 \
+    -DWCSLIB_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/wcslib \
+    -DCFITSIO_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/cfitsio \
+    -DQWT_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/qwt-6.1.0 \
     -DLIBSAKURA_ROOT_DIR=/opt/casa/01/lib/libsakura/default/ \
     -Darch=$TARGETOS -DCMAKE_BUILD_TYPE=Release ..
     ## -DCXX11=1
@@ -400,47 +346,26 @@ else
     # -DWCSLIB_ROOT_DIR=/media/workdrive/CARTA/CARTAvis-externals/ThirdParty/wcslib
     cmake -DUseCrashReporter=0 -DBoost_NO_BOOST_CMAKE=1 '-DEXTRA_C_FLAGS=-DPG_PPU' \
     -DUseCasacoreNamespace=1 \
-    -DWCSLIB_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/wcslib \
-    -DCFITSIO_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/cfitsio \
-    -DQWT_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/qwt-6.1.0 \
+    -DWCSLIB_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/wcslib \
+    -DCFITSIO_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/cfitsio \
+    -DQWT_ROOT_DIR=$cartawork/CARTAvis-externals/ThirdParty/qwt-6.1.0 \
     -DLIBSAKURA_ROOT_DIR=/opt/casa/01/lib/libsakura/default/ \
     -Darch=$TARGETOS -DCMAKE_BUILD_TYPE=Release ..
 fi
 
-# -DCMAKE_Fortran_COMPILER=/opt/casa/02/bin/gfortran-mp-5 \
-### Mac part
-# cmake -DUseCrashReporter=0 -DBoost_NO_BOOST_CMAKE=1 \
-# -Darch=darwin -DCMAKE_BUILD_TYPE=Release -DCXX11=1 \
-# -DWCSLIB_ROOT_DIR=/usr/local/Cellar/wcslib/5.16_1 \
-# -DREADLINE_ROOT_DIR=/usr/local/opt/readline \
-# -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
-# -DCMAKE_C_COMPILER=/usr/bin/clang \
-# -DLLVMCOMPILER=1 \
-# -DINTERACTIVE_ITERATION=1 -Dpgplot_ext=_nox ..
-# # -DCMAKE_INSTALL_PREFIX=../../darwin
-###
-
-# https://safe.nrao.edu/wiki/bin/view/Software/CASA/CartaBuildInstructionsForUbuntu
-# cmake -DUseCrashReporter=0 -DBoost_NO_BOOST_CMAKE=1 '-DEXTRA_C_FLAGS=-DPG_PPU' \
-# -Darch=linux -DCMAKE_BUILD_TYPE=Release \
-# -DCXX11=1 -DWCSLIB_ROOT_DIR=$CARTAWORKHOME/CARTAvis-externals/ThirdParty/wcslib ..
-
 # it may build fail due to no official parallel build support of casa
 make -j2
 
-# Ville said code does not need make install
-# make install
+mkdir -p $cartawork/CARTAvis-externals/ThirdParty/casacore
+mkdir -p $cartawork/CARTAvis-externals/ThirdParty/imageanalysis
 
-mkdir -p $CARTAWORKHOME/CARTAvis-externals/ThirdParty/casacore
-mkdir -p $CARTAWORKHOME/CARTAvis-externals/ThirdParty/imageanalysis
-
-cd $CARTAWORKHOME/CARTAvis-externals/ThirdParty/imageanalysis
-ln -s $CARTAWORKHOME/CARTAvis-externals/ThirdParty/casa/trunk/$TARGETOS/include/casacode/ include
-ln -s $CARTAWORKHOME/CARTAvis-externals/ThirdParty/casa/trunk/$TARGETOS/lib lib
+cd $cartawork/CARTAvis-externals/ThirdParty/imageanalysis
+ln -s $cartawork/CARTAvis-externals/ThirdParty/casa/trunk/$TARGETOS/include/casacode/ include
+ln -s $cartawork/CARTAvis-externals/ThirdParty/casa/trunk/$TARGETOS/lib lib
 
 cd ../casacore
-ln -s $CARTAWORKHOME/CARTAvis-externals/ThirdParty/casa/trunk/$TARGETOS/include/ include
-ln -s $CARTAWORKHOME/CARTAvis-externals/ThirdParty/casa/trunk/$TARGETOS/lib lib
+ln -s $cartawork/CARTAvis-externals/ThirdParty/casa/trunk/$TARGETOS/include/ include
+ln -s $cartawork/CARTAvis-externals/ThirdParty/casa/trunk/$TARGETOS/lib lib
 
 if [ "$isCentOS" = true ] ; then
     unalias qmake
--- carta/scripts/install3party.sh
+++ carta/scripts/install3party.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-## working folder, $CARTAWORKHOME
-export CARTAWORKHOME=`pwd`
+## working folder, $cartawork
 
-##################################
-# Install some required packages
-##################################
-
-## Requirement of carta:
-## g++ >= 4.8.1
-## cfitsio >= 3360
-## wcslib >= 4.23
-## gsl >= 2.1
-## ast >= 8.0.2
-
-#####
-# How to solve portable issues for dynamic libs, e.g. move carta binary or even packaing? Use rpath??
-####
+if [ -z ${cartawork+x} ]; then
+	export cartawork=`pwd`
+else
+fi
 
 isCentOS=true
 if grep -q CentOS /etc/os-release; then
@@ -33,28 +22,14 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$isCentOS" = true ] ; then
 	##### CentOS 7
 
-	## these (wcslib, cfitsio) are required by Carta (but also duplicate install from source),
-	## also are required by casa-submodules (at least casacore)
-	## Todo [Giveup] !!: carta switchs to use yum version which are 3370, 5.15.
-	## carta uses ThirdParty to look for cfitsio, wcslio,
-	## so to use yum version, switch to /usr/lib, /usr/include etc? <-on Linux, default search path
 	sudo yum -y install epel-release ## which has cfitsio, leveldb, old wcslib
-	## to get the same version, so that cfitsio, wcslib are built for carta, casa
 
-	## move to buildcasa
+	## Default wcslib on CentOS 7 is 4.x. But it seems toe become 5.x, after using casa.repo.
+	## wcslib.x86_64 5.15-2.el7.1 @casa
 	# sudo yum -y install wcslib wcslib-devel
-
 	# sudo yum -y install cfitsio cfitsio-devel
 	# -> change to all use build from code, since yum/apt/mac ports have different version
 
-	## Default wcslib on CentOS 7 is 4.x. But it seems that, after using casa.repo.
-	## wcslib.x86_64 5.15-2.el7.1 @casa
-
-	## required by carta, casa-submodue:code
-	# yum:1.15. so carta keeps building it from source code.
-	# Todo [Done]: fix header/lib/dynamic path in Qt, on Mac, CentOS, for Carta.
-	# carta uses /usr/local to look for gsl.
-	# Maybe just build from source and install there, let carta and casa use
 	# sudo yum -y install gsl gsl-devel -> move to buildcasa.sh
 
 	## these are required by carta, also are required by casa-submodules
@@ -64,15 +39,12 @@ elif [ "$isCentOS" = true ] ; then
 	sudo yum -y install Cython
 	sudo yum install python-matplotlib
 
-	## carta only:
-	## ast (static linking)
-
 	## sqlite
 	sudo yum -y install sqlite-devel
 
 	## leveldb
 	# leveldb needs epel-release, so install it first
-	# (nrao-carta does not mention but nrao-casa mention)
+	# (nrao-carta page does not mention but nrao-casa page mention)
 	# second way: curl -O -L  https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7
 	# mv RPM-GPG-KEY-EPEL-7  /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
 	sudo yum -y install leveldb leveldb-devel
@@ -81,6 +53,14 @@ elif [ "$isCentOS" = true ] ; then
 	## needed by building gsl and casacore,
 	## but seems already exist or no need ?
 	sudo yum -y install blas blas-devel
+
+	# for building Qt 4.8.5,
+	# On Grimmer's docker -centos 7.3 , only when buidling Carta by Qt-5.3.2, we need gstreamer-devel, gstreamer-plugins-base
+	# But on Chia-Jung Hsu's pc - centos 7.3, building Qt 4.8.5 needs pkgconfig(gstreamer-app-0.10) which install gstreamer-plugins-base-devel ,
+	#    also gstreamer-devel, gstreamer-plugins-base,  gstreamer
+	# Qt's QtWebkit may use gstreamer libs
+	# We do not build Qt 4.8.5 on CentOS anymore, but building CARTA may need this, so move here
+	sudo yum -y install 'pkgconfig(gstreamer-app-0.10)'
 else
 	##### Ubuntu 16.04
 	# sudo apt-get -y install libcfitsio3-dev
@@ -98,13 +78,14 @@ else
 
 	## needed for compiling qwt
 	apt-get install mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev
+
+	## needed for building CARTA
+	sudo apt-get -y install libgstreamer0.10-dev gstreamer0.10-plugins-base
 fi
 
-
 #####
-
-mkdir -p $CARTAWORKHOME/CARTAvis-externals/ThirdParty
-cd $CARTAWORKHOME/CARTAvis-externals/ThirdParty
+mkdir -p $cartawork/CARTAvis-externals/ThirdParty
+cd $cartawork/CARTAvis-externals/ThirdParty
 
 ## for building qwt by qt5.3 for carta
 if [ -z ${QT5PATH+x} ]; then
@@ -118,17 +99,19 @@ if [ -z ${QT5PATH+x} ]; then
 	export PATH=$QT5PATH:$PATH
 else
 	echo "QT5PATH is already set to '$QT5PATH'";
-	export PATH=$QT5PATH:$PATH
+	export PATH=$QT5PATH/bin:$PATH # Homebrew case
+	export PATH=$QT5PATH/clang_64/bin:$PATH # Qt official installer case
+	export PATH=$QT5PATH/gcc_64/bin:$PATH # Qt official installer case
 fi
 
 curl -O -L http://downloads.sourceforge.net/project/qwt/qwt/6.1.2/qwt-6.1.2.tar.bz2
 tar xvfj qwt-6.1.2.tar.bz2 && mv qwt-6.1.2 qwt-6.1.2-src
 cd qwt-6.1.2-src # can use qwt 6.1.3 Pavol uses
 if [ "$(uname)" == "Darwin" ]; then
-  perl -i -pe 's/.*/ QWT_INSTALL_PREFIX    = $ENV{CARTAWORKHOME}\/CARTAvis-externals\/ThirdParty\/qwt-6.1.2\/ / if $.==22' qwtconfig.pri
+  perl -i -pe 's/.*/ QWT_INSTALL_PREFIX    = $ENV{cartawork}\/CARTAvis-externals\/ThirdParty\/qwt-6.1.2\/ / if $.==22' qwtconfig.pri
 else
 	# for unix part, not work on Mac
-	sed -i "22,22c QWT_INSTALL_PREFIX    = $CARTAWORKHOME/CARTAvis-externals/ThirdParty/qwt-6.1.2" qwtconfig.pri
+	sed -i "22,22c QWT_INSTALL_PREFIX    = $cartawork/CARTAvis-externals/ThirdParty/qwt-6.1.2" qwtconfig.pri
 fi
 qmake qwt.pro
 make && make install
@@ -146,15 +129,16 @@ unzip qooxdoo-3.5-sdk.zip
 
 ## rapidjson
 curl -O -L https://github.com/miloyip/rapidjson/archive/v1.0.2.tar.gz
-tar xvfz v1.0.2.tar.gz
-ln -s rapidjson-1.0.2 rapidjson # can become mv
+mv v1.0.2.tar.gz rapidjson_v1.0.2.tar.gz
+tar xvfz rapidjson_v1.0.2.tar.gz
+ln -s rapidjson-1.0.2 rapidjson
 
+## to get the same version with casa, so cfitsio, wcslib are built for carta, casa
 ## wcslib-5.15
 wget ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-5.15.tar.bz2
 tar xvfj wcslib-5.15.tar.bz2 && mv wcslib-5.15 wcslib-5.15-src
 cd wcslib-5.15-src
-# ./configure --prefix=${THIRDPARTY_PATH}/wcslib/ --without-pgplot <-pgplot ?? <-add back later
-./configure --prefix=`pwd`/../wcslib/  ##--without-pgplot which is needed by casa
+./configure --prefix=`pwd`/../wcslib/  ##--without-pgplot, pgplot is needed by casa
 make && make install
 cd ..
 
@@ -166,7 +150,7 @@ cd cfitsio-src
 make && make install
 cd ..
 
-## ast
+## ast: carta only, static linking with CARTA
 wget http://www.starlink.ac.uk/download/ast/ast-8.0.2.tar.gz
 tar xvfz ast-8.0.2.tar.gz && mv ast-8.0.2 ast-8.0.2-src
 cd ast-8.0.2-src
@@ -175,9 +159,12 @@ make && make install
 cd ..
 
 ## gsl
+# yum:1.15. so carta keeps building it from source code.
+# casa's cmake needs yum version to pass the check but it will use /usr/local in high priority when building
 curl -O -L http://ftp.gnu.org/gnu/gsl/gsl-2.1.tar.gz
 tar xvfz gsl-2.1.tar.gz && mv gsl-2.1 gsl-2.1-src
 cd gsl-2.1-src
 ./configure
-make && make install
+make
+sudo make install
 cd ..
