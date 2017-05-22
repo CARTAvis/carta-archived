fc54518a9c5af929cb80568da9442a98165acaf8
Grimmer <grimmer0125@gmail.com>
2017-05-22 17:36:33 +0800

    Go back to use qooxdoo 3.5.

--- carta/scripts/install3party.sh
+++ carta/scripts/install3party.sh
@@ -126,7 +126,7 @@ fi
 echo "install qooxdoo"
 cd $cartawork/CARTAvis-externals/ThirdParty
 ## Install qooxdoo for CARTA
-curl -o qooxdoo-3.5-sdk.zip -L https://github.com/qooxdoo/qooxdoo/releases/download/release_3_5_1/qooxdoo-3.5.1-sdk.zip
+curl -o qooxdoo-3.5-sdk.zip -L http://downloads.sourceforge.net/project/qooxdoo/qooxdoo-current/3.5/qooxdoo-3.5-sdk.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fqooxdoo%2Ffiles%2Fqooxdoo-current%2F3.5%2F&ts=1479095500&use_mirror=excellmedia
 unzip qooxdoo-3.5-sdk.zip
 
 ## rapidjson
