90d1168797ab8e955a02608b88ed3e54ecf084ec
Kuo-Song Wang <kswang@water.local>
2017-06-03 19:33:54 +0800

    unify font

--- carta/html5/common/skel/source/class/skel/theme/Font.js
+++ carta/html5/common/skel/source/class/skel/theme/Font.js
@@ -17,6 +17,17 @@ qx.Theme.define("skel.theme.Font",
       "default" :
       {
         size : 14
+        family : [ "carta_custom" ],
+        sources:
+        [
+            {
+                family : "carta_custom",
+                source:
+                [
+                    "common/skel/source/class/skel/theme/Roboto-Regular.ttf"
+                ]
+            }
+        ]
       },
       "titleFont" :
           {
