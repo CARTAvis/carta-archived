37cc9e50d5cb81a4247dcfc77f03639b1b3b2236
Adrianna Pińska <adrianna.pinska@gmail.com>
2017-08-08 02:15:02 +0200

    removed old commented-out implementation

--- carta/cpp/plugins/ConversionIntensity/ConverterIntensity.h
+++ carta/cpp/plugins/ConversionIntensity/ConverterIntensity.h
@@ -18,14 +18,6 @@ public:
     static const double LIGHT_SPEED_FACTOR;
     static const double ARCSECONDS_SQUARED_PER_STERADIAN;
     static const double JY_IN_MJY;
-    //static const QString FRACTION_OF_PEAK;
-    //static const QString KELVIN;
-    //static const QString JY_SR;
-    //static const QString JY_ARCSEC;
-    //static const QString JY_BEAM;
-    //static const QString JY;
-    //static const QString TIMES_PIXELS;
-    //static bool isSupportedUnits( const QString& yUnit );
     
     //Hertz values are needed corresponding to the values for Jy/Beam Kelvin conversions
     //only.  Both oldUnits and newUnits refer to the old and new units of the values
@@ -46,46 +38,8 @@ public:
     // Split units into a tuple containing an exponent based on the prefix, and the base units.
     static std::tuple<int, QString> splitUnits(const QString& units);
 
-    //static void convert( std::vector<double> &resultValues, int sourceIndex, int destIndex);
-
-    ////Converts between Jy/Beam units.  For example, MJy/Beam <-> Jy/Beam
-    //static double convertJyBeams( const QString& sourceUnits, const QString& destUnits,
-            //double value );
-    //static double convertJY( const QString& oldUnits, const QString& newUnits,
-            //double value );
-    //static double convertJYSR( const QString& oldUnits,const QString& newUnits,
-            //double value );
-    //static double convertKelvin( const QString& oldUnits,const QString& newUnits,
-            //double value );
     virtual ~ConverterIntensity();
 
 private:
     ConverterIntensity();
-    //static double percentToValue( double yValue, double maxValue );
-    //static double valueToPercent( double yValue, double maxValue );
-    //static double convertQuantity( double yValue, double frequencyValue,
-            //const QString& oldUnits, const QString& newUnits,
-            //double beamSolidAngle, double beamArea );
-    //static void convertJansky( std::vector<double>& values, const QString& oldUnits,
-            //const QString& newUnits );
-    //static void convertKelvin( std::vector<double>& values, const QString& oldUnits,
-            //const QString& newUnits );
-    //static bool isJansky( const QString& units );
-    //static bool isKelvin( const QString& units );
-    //static double convertNonKelvinUnits( double yValue, const QString& oldUnits,
-            //const QString& newUnits, double beamArea );
-    //static QString getJanskyBaseUnits( const QString& units );
-    //static QString getKelvinBaseUnits( const QString& units );
-    //static QString stripPixels( const QString& units );
-    //static double beamToArcseconds( double yValue, double beamArea );
-    //static double arcsecondsToBeam( double yValue, double beamArea );
-    //static double srToArcseconds( double yValue );
-    //static double arcsecondsToSr( double yValue );
-    //static const QList<QString> BEAM_UNITS;
-    //static const QList<QString> JY_UNITS;
-    //static const QList<QString> JY_SR_UNITS;
-    //static const QList<QString> KELVIN_UNITS;
-    //static const double SPEED_LIGHT_FACTOR;
-    //static const double FREQUENCY_FACTOR;
-    //static const double ARCSECONDS_PER_STERADIAN;
 };
