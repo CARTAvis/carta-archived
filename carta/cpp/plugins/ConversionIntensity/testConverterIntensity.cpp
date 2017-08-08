df6399b6253a438eeb1819be5ef8187192f7e394
Adrianna Pińska <adrianna.pinska@gmail.com>
2017-08-08 14:44:07 +0200

    decreased error margin in test; testing absolute error for more cases which are expected to be less lossy; FOP MJy/sr to Kelvin still unusually lossy probably because of ordering of terms

--- carta/cpp/plugins/ConversionIntensity/testConverterIntensity.cpp
+++ carta/cpp/plugins/ConversionIntensity/testConverterIntensity.cpp
@@ -121,26 +121,25 @@ void TestConverterIntensity::convert_data()
                     from_power = n1.second + p1.second;
                     to_power = n2.second + p2.second;
                     prefix_multiplier = pow(10, from_power - to_power);
-                    
-                    bool check_absolute_error = (from_power == to_power && from_power == 0) ? 1 : 0;
-                    
+                                        
                     for (auto& b1 : BASE_UNITS) {
                         for (auto& b2 : BASE_UNITS) {
+
+                            from_units = n1.first + p1.first + b1;
+                            to_units = n2.first + p2.first + b2;
+
+                            double eps = 1e-12;
+                            bool check_absolute_error = (b1 == b2 || from_power == to_power) ? 1 : 0;
+                            
                             if (b1 == b2) { // Within same base units
                                 for (int i = 0; i < 3; i++) {
                                     expected_values[i] = values[i] * prefix_multiplier;
                                 }
-                            } else { // Between different base units          
+                            } else { // Between different base units
                                 for (int i = 0; i < 3; i++) {
                                     expected_values[i] = lambdas[b1][b2](values[i], x_values[i]) * multipliers[b1][b2] * prefix_multiplier;
                                 }
                             }
-
-                            from_units = n1.first + p1.first + b1;
-                            to_units = n2.first + p2.first + b2;
-                            
-                            // The arcsecond/steradian conversion factor in the plugin has low precision, so we lower this for now
-                            double eps = (b1 == "MJy/sr" || b2 == "MJy/sr") ? 1e-3 : 1e-5;
 
                             row_name = from_units + " to " + to_units;
                             QTest::newRow(row_name.toLatin1().data()) << from_units << to_units << from_units << values << x_values << max_value << expected_values << check_absolute_error << eps;
@@ -177,19 +176,19 @@ void TestConverterIntensity::convert()
     double error;
     double percentage_error;
     
-    std::string failure_message;
+    QString failure_message;
     
     ConverterIntensity::convert( values, x_values, from_units, to_units, max_value, max_units, BEAM_SOLID_ANGLE, BEAM_AREA );
     
     for (size_t i = 0; i < values.size(); i++) {
         error = fabs(values[i] - expected_values[i]);
         percentage_error = error * 100 / expected_values[i];
-        failure_message = "Error is " + std::to_string(error) + " (" + std::to_string(percentage_error) + "%)";
-        // Checking the percentile error for now, to distinguish actual failures from loss of precision.
-        QVERIFY2(percentage_error < eps, failure_message.c_str());
-        // But also at least check absolute error for conversions between units with no prefix
+        failure_message = "Expected: " + QString::number(expected_values[i], 'e', 2) + " Actual: " + QString::number(values[i], 'e', 2) + "Error is " + QString::number(error, 'e', 2) + " (" + QString::number(percentage_error, 'e', 2) + "%)";
+        // Checking the percentile error for all conversions
+        QVERIFY2(percentage_error < eps, failure_message.toLatin1().data());
+        // Check absolute error for some conversions which should not accumulate too much loss of precision
         if (check_absolute_error) {
-            QVERIFY2(error < eps, failure_message.c_str());
+            QVERIFY2(error < eps, failure_message.toLatin1().data());
         }
     }
 }
