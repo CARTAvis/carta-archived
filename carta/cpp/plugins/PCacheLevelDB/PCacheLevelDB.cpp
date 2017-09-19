5165109145cc78c4791157aadfb4f177b7b489cf
mark <chcchiang@asiaa.sinica.edu.tw>
2017-09-19 12:20:20 +0800

    comment a unnecessary code line in PCacheLevelDB.cpp

--- carta/cpp/plugins/PCacheLevelDB/PCacheLevelDB.cpp
+++ carta/cpp/plugins/PCacheLevelDB/PCacheLevelDB.cpp
@@ -76,7 +76,7 @@ public:
             qWarning() << "query insert failed:" << status.ToString().c_str();
         }
 
-        Q_UNUSED( priority);
+        //Q_UNUSED( priority);
         // \todo we'll have to embed  priority into the value, e.g. first 8 bytes?
     } // setEntry
 
