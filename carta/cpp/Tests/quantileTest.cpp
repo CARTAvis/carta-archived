aac7d2eac7ebf4ce6d096e8fd265e1acd5a7d771
Adrianna Pińska <adrianna.pinska@gmail.com>
2017-09-20 16:17:45 +0200

    trying to fix bug; unexpected behaviour of priority queuewq

--- carta/cpp/Tests/quantileTest.cpp
+++ carta/cpp/Tests/quantileTest.cpp
@@ -2,27 +2,36 @@
 #include "TestRawView.h"
 #include "core/Algorithms/percentileAlgorithms.h"
 #include "core/Algorithms/percentileManku99.h"
-#include <QDebug>
+#include <random>
 
 TEST_CASE( "Quantile algorithm test", "[quantile]" ) {
-    std::vector<double> data = {1,2,3,4,5,6,7,8,9,10,11,12};
-    std::vector<int> dims = {2,3,2};
+    
+    std::uniform_real_distribution<double> dist(0, 100);
+    std::default_random_engine gen;
+
+    std::vector<int> dims = {200, 200, 10};
+    std::vector<double> data(200 * 200 * 10);
+    std::generate(data.begin(), data.end(), [&]() { return dist(gen); });
     
     Carta::Lib::NdArray::RawViewInterface * rawView = new TestRawView<double>(data, dims);
     Carta::Lib::NdArray::Double doubleView(rawView, false);
         
     const std::vector<double> percentiles = {0, 0.01, 0.1, 0.9, 0.99, 1};
-
+        
     SECTION( "Exact algorithm") {
         std::map <double, double> intensities = Carta::Core::Algorithms::percentile2pixels(doubleView, percentiles);
-        qDebug() << intensities;
-        //REQUIRE( ??? );
+        REQUIRE(intensities.size() == percentiles.size());
+        for (auto& kv : intensities) {
+            qDebug() << kv.first << ":" << kv.second;
+        }
     }
 
     SECTION( "Manku 99 algorithm") {
         std::map <double, double> intensities =  Carta::Core::Algorithms::percentile2pixels_approximate_manku99(doubleView, percentiles);
-        qDebug() << intensities;
-        //REQUIRE( ??? );
+        REQUIRE(intensities.size() == percentiles.size());
+        for (auto& kv : intensities) {
+            qDebug() << kv.first << ":" << kv.second;
+        }
     }
 
     delete rawView;
--- carta/cpp/core/Algorithms/percentileManku99.h
+++ carta/cpp/core/Algorithms/percentileManku99.h
@@ -57,11 +57,13 @@ public:
     }
     
     /** NEW operation */
-    void opNew(min_heap<Scalar>& elements, size_t weight, size_t level, State state) {        
+    void opNew(min_heap<Scalar>& elements, size_t weight, size_t level, State state) {
+        qDebug() << "in opNew";
         this->elements = std::move(elements);
         this->state = state;
         this->weight = weight;
         this->level = level;
+        qDebug() << "end of opNew";
     }
 
     /** Weighted buffer merge */
@@ -71,7 +73,7 @@ public:
         std::function<size_t(size_t)> nextIndexLambda,
         std::function<void(size_t, Scalar)> processValueLambda
     ) {
-
+        qDebug() << "in merge";
         // buffers which have elements left
         std::vector<Buffer<Scalar>*> remainingBuffers;
         for (auto& b : inputBuffers) {
@@ -81,7 +83,7 @@ public:
         // the index of the buffer with the overall lowest next value
         size_t minBufferIndex;
         // the overall lowest next value
-        Scalar minNextVal(std::numeric_limits<Scalar>::infinity());
+        Scalar minNextVal;
         // the corresponding buffer weight
         size_t weight;
 
@@ -93,6 +95,9 @@ public:
         // while the destination element buffer isn't full
         while (!stopIterationLambda()) {
             // find the buffer with the lowest overall next value
+            
+            minNextVal = std::numeric_limits<Scalar>::infinity();
+            
             for (size_t i = 0; i < remainingBuffers.size(); i++) {
                 if (remainingBuffers[i]->elements.top() < minNextVal) {
                     minBufferIndex = i;
@@ -101,7 +106,7 @@ public:
                     weight = remainingBuffers[i]->weight;
                 }
             }
-
+            
             // actually pop the value here
             remainingBuffers[minBufferIndex]->elements.pop();
 
@@ -111,11 +116,11 @@ public:
             if (!remainingBuffers[minBufferIndex]->elements.size()) {
                 remainingBuffers.erase(remainingBuffers.begin() + minBufferIndex);
             }
-
+            
             // I'm sure there's a cleaner way to do this
             // if any samples fall within the next [weight] elements of the expanded list, use this value for those samples
             for (size_t p = pos; p < pos + weight; p++) {
-                if (p == nextIndex) {
+                if (p == nextIndex) { 
                     processValueLambda(nextIndex, minNextVal);
                     nextIndex = nextIndexLambda(nextIndex);
                 }
@@ -123,10 +128,12 @@ public:
             // actually advance our position in the expanded list
             pos += weight;
         }
+        qDebug() << "end of merge";
     }
 
     /** COLLAPSE operation */
     static void opCollapse(std::vector<Buffer<Scalar>*> & inputBuffers) {
+        qDebug() << "in opCollapse";
         // Weight of collapsed buffer is the sum of the weights of all input buffers
         size_t YWeight = std::accumulate (begin(inputBuffers), end(inputBuffers), 0, [](size_t a, Buffer<Scalar>*& b){ return b->weight + a; });
         // Level of collapsed buffer is one more than the level of each input buffer
@@ -177,43 +184,63 @@ public:
         Y->weight = YWeight;
         Y->state = State::full;
         Y->level = YLevel;
+        qDebug() << "end of opCollapse";
     }
     
 
     /** OUTPUT operation */
     static std::map<double, Scalar> opOutput(std::vector<Buffer<Scalar>*> inputBuffers, const std::vector<double> quantiles) {
+        qDebug() << "in opOutput";
         // kW is the sum of the weight x actual size of all input buffers
         size_t kW = std::accumulate (begin(inputBuffers), end(inputBuffers), 0, [](size_t a, Buffer<Scalar>*& b){ return (b->size() * b->weight) + a; });
 
+        // Quantiles close together may have the same index
+        // We need to eliminate the duplicates to avoid breaking the merge algorithm
+        std::set<size_t> indicesSet;
         min_heap<size_t> indices;
-        std::map<size_t, double> quantileForIndex;
+        std::map<size_t, std::vector<double> > quantilesForIndex;
         
         for (auto & phi : quantiles) {
-            size_t index = ceil(phi * kW) - 1;
-            indices.push(index);
-            quantileForIndex[index] = phi;
+            size_t index = (size_t) std::max(ceil(phi * kW) - 1, 0.0);
+            if (indicesSet.find(index) == indicesSet.end()) {
+                indicesSet.insert(index);
+                indices.push(index);
+            }
+            quantilesForIndex[index].push_back(phi);
         }
-
-        auto stopIterationLambda = [&indices] () {
-            return indices.empty();
+        
+        bool stopIteration(false);
+        
+        auto stopIterationLambda = [&stopIteration] () {
+            return stopIteration;
         };
 
-        auto nextIndexLambda = [&indices] (size_t lastIndex) {
+        auto nextIndexLambda = [&indices, &stopIteration] (size_t lastIndex) {
             Q_UNUSED(lastIndex);
-            size_t next = indices.top();
-            indices.pop();
+            size_t next(0);
+            if (indices.empty()) {
+                stopIteration = true;
+            } else {
+                next = indices.top();
+                indices.pop();
+            }
             return next;
         };
 
         std::map<double, Scalar> values;
 
-        auto processValueLambda = [&quantileForIndex, &values] (size_t index, Scalar value) {
-            values[quantileForIndex[index]] = value;
+        auto processValueLambda = [&quantilesForIndex, &values] (size_t index, Scalar value) {
+            for (auto& q : quantilesForIndex[index]) {
+                values[q] = value;
+            }
         };
         
         // perform the weighted merge
-        merge(inputBuffers, 0, stopIterationLambda, nextIndexLambda, processValueLambda);
+        size_t startIndex = indices.top();
+        indices.pop();
+        merge(inputBuffers, startIndex, stopIterationLambda, nextIndexLambda, processValueLambda);
 
+        qDebug() << "end of opOutput";
         return values;
     }
 };
@@ -235,6 +262,7 @@ percentile2pixels_approximate_manku99(
     size_t sampleAfter=10
     )
 {
+    
     // basic preconditions
     if ( CARTA_RUNTIME_CHECKS ) {
         for ( auto q : percentiles ) {
@@ -296,20 +324,28 @@ percentile2pixels_approximate_manku99(
             if (empty.size()) {                    
                 elementBlock.push_back(val);
                 hzForBlock.push_back(hzVal);
+                
+                qDebug() << "sampling rate is" << samplingRate << "and element block size is" << elementBlock.size();
 
                 if (elementBlock.size() == samplingRate) {
+                    qDebug() << "selecting element from block of size" << elementBlock.size();
                     // select random element from block, do unit conversion and push to elements
                     ri = dist(mt);
+                    qDebug() << "got random index" << ri;
+                    qDebug() << "selected value is" << conversion_lambda(ri);
                     bufferElements.push(conversion_lambda(ri));
                     elementBlock.clear();
                     hzForBlock.clear();
                     
+                    qDebug() << "size of temp buffer is now" << bufferElements.size();
                     // create new buffer whenever elements reach buffer capacity
                     // we'll do it once more at the end to account for a possible partial buffer
                     if (bufferElements.size() == bufferCapacity) { // NEW operation
+                        qDebug() << "preparing to call new with temp buffer of size" << bufferElements.size();
                         Buffer<Scalar> *& newBuffer = empty.front();
                         newBuffer->opNew(bufferElements, samplingRate, newBufferLevel, Buffer<Scalar>::State::full);
-                        if (fullLevelSet.find(newBufferLevel) != fullLevelSet.end()) {
+                        if (fullLevelSet.find(newBufferLevel) == fullLevelSet.end()) {
+                            qDebug() << "adding level" << newBufferLevel << "to set and pushing it to heap";
                             // add the level to the set
                             fullLevelSet.insert(newBufferLevel);
                             // add the level to the heap
@@ -320,36 +356,60 @@ percentile2pixels_approximate_manku99(
                         // remove buffer from list of empty buffers
                         empty.pop_front();
                     }
+                    qDebug() << "number of empty buffers:" << empty.size();
+                    int tmp(0);
+                    for (auto& kv : full) {
+                        qDebug() << kv.second.size() << "buffers at level" << kv.first;
+                        tmp += kv.second.size();
+                    }
+                    qDebug() << "total number of full buffers:" << tmp;
                 }
             } else { // COLLAPSE operation
                 // Find full buffers with the lowest level
+                //qDebug() << "HEAP CONTENTS";
+                //for (size_t l = 0; l < fullLevelHeap.size(); l++) {
+                    //qDebug() << fullLevelHeap[l];
+                //}
+                qDebug() << "BEFORE POP, TOP IS" << fullLevelHeap.top();
+                qDebug() << "BEFORE POP, SIZE IS" << fullLevelHeap.size();
                 lowestFullLevel = fullLevelHeap.top();
+                qDebug() << "lowest full buffer level" << lowestFullLevel;
                 fullLevelHeap.pop();
                 fullLevelSet.erase(lowestFullLevel);
                 lowest = full[lowestFullLevel];
                 full.erase(lowestFullLevel);
-
+                qDebug() << "num of buffers at this level" << lowest.size();
+                
+                qDebug() << "AFTER POP, TOP IS" << fullLevelHeap.top();
+                qDebug() << "AFTER POP, SIZE IS" << fullLevelHeap.size();
+                
                 // If there is only one,
                 if (lowest.size() == 1) {
                     // add the next lowest buffer(s)
                     secondLowestFullLevel = fullLevelHeap.top();
+                    qDebug() << "second lowest full buffer level" << secondLowestFullLevel;
                     fullLevelHeap.pop();
                     fullLevelSet.erase(secondLowestFullLevel);
                     secondLowest = full[secondLowestFullLevel];
                     full.erase(secondLowestFullLevel);
+                    qDebug() << "num of buffers at this level" << secondLowest.size();
                     // and promote the lowest buffer
                     lowest[0]->level = secondLowestFullLevel;
                     secondLowest.push_back(lowest[0]);
                     lowest = std::move(secondLowest);
+                    qDebug() << "now num of buffers at lowest level" << lowest.size();
                 }
 
                 // perform the collapse
                 Buffer<Scalar>::opCollapse(lowest);
+                qDebug() << "after opCollapse";
 
                 // all buffers after the first are now empty
                 for (size_t i = 1; i < lowest.size(); i++) {
                     empty.push_back(lowest[i]);
                 }
+                
+                qDebug() << "number of empty buffers:" << empty.size();
                 
                 // the first buffer is full and one level higher
                 if (fullLevelSet.find(lowest[0]->level) != fullLevelSet.end()) {
@@ -361,15 +421,27 @@ percentile2pixels_approximate_manku99(
                 // add the buffer to the map of full buffers
                 full[lowest[0]->level].push_back(lowest[0]);
                 
+                int tmp(0);
+                for (auto& kv : full) {
+                    qDebug() << kv.second.size() << "buffers at level" << kv.first;
+                    tmp += kv.second.size();
+                }
+                qDebug() << "total number of full buffers:" << tmp;
+                
                 // update tree height
                 height = std::max(height, lowest[0]->level);
 
+                qDebug() << "after updating height to" << height;
+                
                 // update new buffer level, sampling rate and random distribution
                 if (height >= sampleAfter) {
                     newBufferLevel = height - sampleAfter + 1;
                     samplingRate = pow(2, newBufferLevel);
                     dist = std::uniform_int_distribution<>(0, samplingRate - 1);
                 }
+                
+                qDebug() << "sampling rate is now" << samplingRate;
+                qDebug() << "newBufferLevel is" << newBufferLevel;
             }
         }
     };
@@ -405,6 +477,7 @@ percentile2pixels_approximate_manku99(
         };
         
         // we can loop over the flat image
+        
         view.forEach(view_lambda);
     }
 
@@ -435,7 +508,7 @@ percentile2pixels_approximate_manku99(
     // find the non-empty buffers
     
     std::vector<Buffer<Scalar>*> nonEmptyBuffers;
-
+    
     while (!fullLevelHeap.empty()) {
         lowestFullLevel = fullLevelHeap.top();
         fullLevelHeap.pop();
