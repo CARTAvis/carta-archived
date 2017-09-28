e191f9cc4376d9b0db5a715a65bc5a2ac167b5f1
Adrianna Pińska <adrianna.pinska@gmail.com>
2017-09-28 15:51:58 +0200

    implementing very basic slice support in stub to extend test

--- carta/cpp/Tests/quantileTest.cpp
+++ carta/cpp/Tests/quantileTest.cpp
@@ -4,6 +4,8 @@
 #include "core/Algorithms/percentileManku99.h"
 #include <random>
 
+#include "CartaLib/Slice.h"
+
 TEST_CASE( "Quantile algorithm test", "[quantile]" ) {
     
     std::uniform_real_distribution<double> dist(0, 100);
@@ -12,6 +14,8 @@ TEST_CASE( "Quantile algorithm test", "[quantile]" ) {
     std::vector<int> dims = {200, 200, 10};
     int size = std::accumulate (begin(dims), end(dims), 1, [](int a, int& b){ return b*a; });
     std::vector<double> data(size);
+    
+    // TODO: add a seed to make this deterministic
     std::generate(data.begin(), data.end(), [&]() { return dist(gen); });
     
     Carta::Lib::NdArray::RawViewInterface * rawView = new TestRawView<double>(data, dims);
@@ -33,6 +37,32 @@ TEST_CASE( "Quantile algorithm test", "[quantile]" ) {
         for (auto& kv : intensities) {
             qDebug() << kv.first << ":" << kv.second;
         }
+    }
+    
+    SECTION("testing frame slice implementation") {
+        SliceND frame;
+        int spectralIndex = 2;
+        int frameIndex = 5;
+        
+        for (size_t d = 0; d < dims.size(); d++) {
+            if ((int)d == spectralIndex) {
+                frame.index(frameIndex);
+            } else {
+                frame.next();
+            }
+        }
+        
+        //qDebug() << frame.slice(0).isNull();
+        //qDebug() << frame.slice(1).isNull();
+        //qDebug() << frame.slice(2).isNull();
+        
+        SliceND::ApplyResult ar = frame.apply( dims);
+        
+        qDebug() << ar.toStr();
+        qDebug() << ar.dims()[0].toStr();
+        qDebug() << ar.dims()[1].toStr();
+        qDebug() << ar.dims()[2].toStr();
+        qDebug() << ar.dims()[2].start;
     }
 
     delete rawView;
