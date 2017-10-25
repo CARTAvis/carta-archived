6b6e7e054219aa2b5d858f07995d9cd4ff45a016
markccchiang <chengchin.chiang@gmail.com>
2017-10-25 23:32:33 +0800

    hard coding colormaps from matplotlib python into CARTA as cpp syntax

--- carta/cpp/CartaLib/PWLinear.h
+++ carta/cpp/CartaLib/PWLinear.h
@@ -54,10 +54,16 @@ public:
         // find the segment and linearly interpolate within it
         for ( int i = 1 ; i < m_points.size() ; i++ ) {
             if ( x <= m_points[i].x() ) {
-                double a =
-                    ( m_points[i - 1].y() - m_points[i].y() ) / ( m_points[i - 1].x() - m_points[i].x() );
-                double b = m_points[i].y() - a * m_points[i].x();
-                return a * x + b;
+                if ( fabs( m_points[i - 1].x() - m_points[i].x() ) < 1.0e-6 ) {
+                    // if the slope of segment is close to infinity, then we choose the previous "y" value
+                    return m_points[i - 1].y();
+                } else {
+                    // otherwise we use the interpolation to get "y" value with respect to the "x"
+                    double a =
+                        ( m_points[i - 1].y() - m_points[i].y() ) / ( m_points[i - 1].x() - m_points[i].x() );
+                    double b = m_points[i].y() - a * m_points[i].x();
+                    return a * x + b;
+                }
             }
         }
         return std::numeric_limits < double >::quiet_NaN();
