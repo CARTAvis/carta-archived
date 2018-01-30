039eb7ad8e338b7d1a85357b98f66b786ed77b11
Jamie Dassoulas <james.dassoulas@gmail.com>
2018-01-30 10:48:17 -0800

    import interactive clean skeleton (adapted from statistics module)

---
+++ carta/cpp/core/Data/InteractiveClean/InteractiveClean.cpp
@@ -0,0 +1,281 @@
+
+#include "InteractiveClean.h"
+#include "Data/Settings.h"
+#include "Data/LinkableImpl.h"
+#include "Data/Image/Controller.h"
+#include "Data/Image/DataSource.h"
+#include "Data/Region/Region.h"
+#include "Data/Region/RegionControls.h"
+#include "Data/Error/ErrorManager.h"
+#include "Data/Util.h"
+#include "CartaLib/Regions/IRegion.h"
+#include "CartaLib/Hooks/ImageStatisticsHook.h"
+#include "State/UtilState.h"
+#include "Globals.h"
+#include <QDebug>
+
+
+namespace Carta {
+
+namespace Data {
+
+const QString InteractiveClean::CLASS_NAME = "InteractiveClean";
+const QString InteractiveClean::FROM = "from";
+const QString InteractiveClean::LABEL = "label";
+const QString InteractiveClean::SELECTED_INDEX = "selectedIndex";
+const QString InteractiveClean::TO = "to";
+
+class InteractiveClean::Factory : public Carta::State::CartaObjectFactory {
+public:
+
+    Carta::State::CartaObject * create (const QString & path, const QString & id)
+    {
+        return new InteractiveClean (path, id);
+    }
+};
+
+bool InteractiveClean::m_registered =
+        Carta::State::ObjectManager::objectManager()->registerClass ( CLASS_NAME, new InteractiveClean::Factory());
+
+using Carta::State::UtilState;
+using Carta::State::StateInterface;
+
+InteractiveClean::InteractiveClean( const QString& path, const QString& id):
+            CartaObject( CLASS_NAME, path, id ),
+            m_linkImpl( new LinkableImpl( path )),
+            //Store region and image selection
+            m_stateData( UtilState::getLookup(path, StateInterface::STATE_DATA )){
+
+    Carta::State::ObjectManager* objMan = Carta::State::ObjectManager::objectManager();
+    Settings* settingsObj = objMan->createObject<Settings>();
+    m_settings.reset( settingsObj );
+
+    _initializeDefaultState();
+    _initializeCallbacks();
+}
+
+
+QString InteractiveClean::addLink( CartaObject*  target){
+    Controller* controller = dynamic_cast<Controller*>(target);
+    bool linkAdded = false;
+    QString result;
+    if ( controller != nullptr ){
+        if ( m_controllerLinked ){
+            //See if it is the one we are already linked to.
+            CartaObject* obj = m_linkImpl->searchLinks( target->getPath());
+            if ( obj != nullptr ){
+                linkAdded = true;
+            }
+            else {
+                //Clear out any existing link so we can add the new one.
+                _clearLinks();
+            }
+        }
+        if ( !linkAdded ){
+            linkAdded = m_linkImpl->addLink( controller );
+            if ( linkAdded ){
+                connect(controller, SIGNAL(dataChanged(Controller*)),
+                        this , SLOT(_updateInteractiveClean(Controller*)));
+                connect( controller, SIGNAL( frameChanged(Controller*, Carta::Lib::AxisInfo::KnownType)),
+                        this, SLOT(_updateInteractiveClean(Controller*, Carta::Lib::AxisInfo::KnownType)));
+                connect(controller, SIGNAL(dataChangedRegion(Controller*)),
+                        this, SLOT( _updateInteractiveClean( Controller*)));
+                m_controllerLinked = true;
+                _updateInteractiveClean( controller, Carta::Lib::AxisInfo::KnownType::OTHER );
+            }
+        }
+    }
+    else {
+        result = "InteractiveClean only supports linking to images";
+    }
+    return result;
+}
+
+void InteractiveClean::_clearLinks(){
+    int linkCount = m_linkImpl->getLinkCount();
+    for ( int i = linkCount-1; i >= 0; i-- ){
+        CartaObject* linkObj = m_linkImpl->getLink(i);
+        removeLink( linkObj );
+    }
+}
+
+
+QList<QString> InteractiveClean::getLinks() const {
+    return m_linkImpl->getLinkIds();
+}
+
+QString InteractiveClean::_getPreferencesId() const {
+    QString id;
+    if ( m_settings.get() != nullptr ){
+        id = m_settings->getPath();
+    }
+    return id;
+}
+
+QString InteractiveClean::getStateString( const QString& sessionId, SnapshotType type ) const{
+    QString result("");
+    if ( type == SNAPSHOT_PREFERENCES ){
+        StateInterface prefState( "");
+        prefState.setValue<QString>(Carta::State::StateInterface::OBJECT_TYPE, CLASS_NAME );
+
+        prefState.insertValue<QString>(Util::PREFERENCES, m_state.toString());
+        prefState.insertValue<QString>( Settings::SETTINGS, m_settings->getStateString(sessionId, type) );
+        result = prefState.toString();
+    }
+    else if ( type == SNAPSHOT_LAYOUT ){
+        result = m_linkImpl->getStateString(getIndex(), getSnapType( type ));
+    }
+    return result;
+}
+
+
+void InteractiveClean::_initializeCallbacks(){
+
+    addCommandCallback( "buttonPressed", [=] (const QString & /*cmd*/,
+                                                    const QString & params, const QString & /*sessionId*/) -> QString {
+                          std::set<QString> keys = {"button"};
+                          std::map<QString,QString> dataValues = Carta::State::UtilState::parseParamMap( params, keys );
+                          QString maskStr = dataValues[*keys.begin()];
+                          QString result = maskStr;
+                          qDebug() << result;
+                          return result;
+     });
+
+    addCommandCallback( "registerPreferences", [=] (const QString & /*cmd*/,
+                                                    const QString & /*params*/, const QString & /*sessionId*/) -> QString {
+                          QString result = _getPreferencesId();
+                          return result;
+     });
+
+
+}
+
+
+
+void InteractiveClean::_initializeDefaultState(){
+    //Data state is the selected image and image/region interactive clean
+    m_stateData.insertValue<int>(SELECTED_INDEX, 0 );
+    m_stateData.flushState();
+
+    m_state.flushState();
+}
+
+
+bool InteractiveClean::isLinked( const QString& linkId ) const {
+    bool linked = false;
+    CartaObject* obj = m_linkImpl->searchLinks( linkId );
+    if ( obj != nullptr ){
+        linked = true;
+    }
+    return linked;
+}
+
+
+QString InteractiveClean::removeLink( CartaObject* cartaObject){
+    bool removed = false;
+    QString result;
+    Controller* controller = dynamic_cast<Controller*>( cartaObject );
+    if ( controller != nullptr ){
+        removed = m_linkImpl->removeLink( controller );
+        if ( removed ){
+            controller->disconnect(this);
+            m_controllerLinked = false;
+        }
+    }
+    else {
+       result = "InteractiveClean was unable to remove link only image links are supported";
+    }
+    return result;
+}
+
+void InteractiveClean::resetState( const QString& state ){
+    StateInterface restoredState( "");
+    restoredState.setState( state );
+
+    QString settingStr = restoredState.getValue<QString>(Settings::SETTINGS);
+    m_settings->resetStateString( settingStr );
+
+    QString prefStr = restoredState.getValue<QString>(Util::PREFERENCES);
+    m_state.setState( prefStr );
+    m_state.setValue<bool>( "flush", true );
+    m_state.flushState();
+    m_state.setValue<bool>("flush", false );
+}
+
+
+void InteractiveClean::_updateInteractiveClean( Controller* controller, Carta::Lib::AxisInfo::KnownType /*type*/  ){
+    if ( controller != nullptr ){
+
+        // int selectedIndex = controller->getSelectImageIndex();
+        // m_stateData.setValue<int>(SELECTED_INDEX, selectedIndex );
+
+        // std::vector< std::shared_ptr<Carta::Lib::Image::ImageInterface> > dataSources =
+        //         controller->getImages();
+
+
+        // std::vector<std::shared_ptr<Region> > coreRegions;
+        // std::shared_ptr<RegionControls> regionControls = controller->getRegionControls();
+        // if ( regionControls ){
+        // 	coreRegions = regionControls->getRegions();
+        // }
+        // int regionCount = coreRegions.size();
+        // std::vector<std::shared_ptr<Carta::Lib::Regions::RegionBase> > regions;
+        // for ( int i = 0; i < regionCount; i++ ){
+        //     regions.push_back( coreRegions[i]->getModel() );
+        // }
+
+        // std::vector<int> frameIndices = controller->getImageSlice();
+
+        // int sourceCount = dataSources.size();
+        // if ( sourceCount > 0 ){
+        //     auto result = Globals::instance()-> pluginManager()
+        //                  -> prepare <Carta::Lib::Hooks::ImageInteractiveCleanHook>(dataSources, regions, frameIndices);
+        //     auto lam = [=] ( const Carta::Lib::Hooks::ImageInteractiveCleanHook::ResultType &data ) {
+
+        //         //An array for each image
+        //         int dataCount = data.size();
+        //         m_stateData.resizeArray( STATS, dataCount );
+        //         for ( int i = 0; i < dataCount; i++ ){
+        //             //Each element of the image array contains an array of interactive clean.
+        //             QString arrayLookup = UtilState::getLookup( STATS, i );
+        //             int statCount = data[i].size();
+        //             m_stateData.setArray( arrayLookup, statCount );
+
+        //             //Go through each set of interactive clean for the image.
+        //             for ( int k = 0; k < statCount; k++ ){
+        //                 QString objLookup = UtilState::getLookup( arrayLookup, k );
+        //                 QList<QString> existingKeys = m_stateData.getMemberNames( objLookup );
+        //                 int keyCount = data[i][k].size();
+        //                 for ( int j = 0; j < keyCount; j++ ){
+        //                     QString label = data[i][k][j].getLabel();
+        //                     QString lookup = UtilState::getLookup( objLookup, label );
+        //                     m_stateData.insertValue<QString>( lookup, data[i][k][j].getValue() );
+        //                 }
+        //             }
+        //         }
+        //         m_stateData.flushState();
+        //     };
+        //     try {
+        //         result.forEach( lam );
+        //     }
+        //     catch( char*& error ){
+        //         QString errorStr( error );
+        //         ErrorManager* hr = Util::findSingletonObject<ErrorManager>();
+        //         hr->registerError( errorStr );
+        //     }
+        // }
+        // //No interactive clean
+        // else {
+        //     m_stateData.resizeArray( STATS, 0 );
+        //     m_stateData.flushState();
+        // }
+
+    }
+}
+
+
+InteractiveClean::~InteractiveClean(){
+
+}
+}
+}
---
+++ carta/cpp/core/Data/InteractiveClean/InteractiveClean.h
@@ -0,0 +1,105 @@
+/***
+ * Manages interactive clean state
+ *
+ */
+
+#pragma once
+
+#include "State/ObjectManager.h"
+#include "State/StateInterface.h"
+#include "Data/ILinkable.h"
+#include "CartaLib/AxisInfo.h"
+
+#include <QObject>
+
+
+namespace Carta {
+
+
+namespace Data {
+
+class Controller;
+class LinkableImpl;
+class Settings;
+
+class InteractiveClean : public QObject, public Carta::State::CartaObject, public ILinkable {
+
+    Q_OBJECT
+
+public:
+
+    //ILinkable
+    QString addLink( CartaObject* cartaObject) Q_DECL_OVERRIDE;
+    QString removeLink( CartaObject* cartaObject) Q_DECL_OVERRIDE;
+    virtual QList<QString> getLinks() const Q_DECL_OVERRIDE;
+
+    /**
+     * Return a string representation of the interactive clean state.
+     * @param sessionId - an identifier for a user session.
+     * @param type - the type of state requested.
+     */
+    virtual QString getStateString( const QString& sessionId, SnapshotType type ) const Q_DECL_OVERRIDE;
+
+    /**
+     * Returns whether or not the object with the given id is already linked to this object.
+     * @param linkId - a QString identifier for an object.
+     * @return true if this object is already linked to the one identified by the id; false otherwise.
+     */
+    virtual bool isLinked( const QString& linkId ) const Q_DECL_OVERRIDE;
+
+    /**
+     * Reset the state of interactive clean.
+     * @param state - the new interactive clean state.
+     */
+    virtual void resetState( const QString& state ) Q_DECL_OVERRIDE;
+
+    virtual ~InteractiveClean();
+    const static QString CLASS_NAME;
+
+
+private slots:
+    /**
+     * Recompute the interactive clean.
+     * @param controller - the controller to use for interactive clean generation.
+     */
+    void _updateInteractiveClean( Controller* controller, Carta::Lib::AxisInfo::KnownType type = Carta::Lib::AxisInfo::KnownType::SPECTRAL );
+
+private:
+    const static QString FROM;
+    const static QString LABEL;
+    const static QString SELECTED_INDEX;
+    const static QString TO;
+
+    void _clearLinks();
+
+    QString _getPreferencesId() const;
+    QString _getStatType( const QString& typeStr ) const;
+
+    void _initializeCallbacks();
+    void _initializeDefaultState();
+    void _initializeLabel( const QString& arrayName, int arrayIndex, const QString& label, bool visible);
+
+
+    static bool m_registered;
+
+    //For right now we are supporting only one linked controller.
+    bool m_controllerLinked;
+
+    InteractiveClean( const QString& path, const QString& id );
+    class Factory;
+
+
+    //Link management
+    std::unique_ptr<LinkableImpl> m_linkImpl;
+
+    //Preference settings
+    std::unique_ptr<Settings> m_settings;
+
+
+    Carta::State::StateInterface m_stateData;
+
+	InteractiveClean( const InteractiveClean& other);
+	InteractiveClean& operator=( const InteractiveClean& other );
+};
+}
+}
