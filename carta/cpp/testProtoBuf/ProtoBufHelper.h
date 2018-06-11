5376b3f1b533bb2f0a1c5c9d0d6839b78326febf
markccchiang <chengchin.chiang@gmail.com>
2018-06-11 15:05:49 +0800

    convert float of pixel value to an array of bytes, and send a single array with length N*4 (where N is the number of float values)

--- carta/cpp/testProtoBuf/ProtoBufHelper.h
+++ carta/cpp/testProtoBuf/ProtoBufHelper.h
@@ -8,6 +8,8 @@
 
 typedef ::google::protobuf::uint32 uint32;
 
+void concentrate(std::string pixel, std::vector<char> &binaryPayloadCache);
+
 void PromptForRasterImageData(Stream::RasterImageData* RasterImageData, uint32 file_id, uint32 layer_id, uint32 x, uint32 y,
                               uint32 width, uint32 height, uint32 stokes, uint32 channel, uint32 mip,
                               Stream::RasterImageData::CompressionType type, float compression_quality, uint32 num_subsets,
